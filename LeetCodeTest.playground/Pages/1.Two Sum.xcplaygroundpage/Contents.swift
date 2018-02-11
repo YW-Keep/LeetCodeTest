4381f38ac78e01e820465173ac7089418a35a4d2
tangyuwei <tangyuwei@myhexin.com>
2018-02-11 10:40:45 +0800

    首次提交

--- LeetCodeTest.playground/Pages/1.Two Sum.xcplaygroundpage/Contents.swift
+++ LeetCodeTest.playground/Pages/1.Two Sum.xcplaygroundpage/Contents.swift
@@ -1,5 +1,29 @@
-//: Playground - noun: a place where people can play
-
+/*
+ Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ 
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ 
+ Example:
+ Given nums = [2, 7, 11, 15], target = 9,
+ 
+ Because nums[0] + nums[1] = 2 + 7 = 9,
+ return [0, 1].
+ */
 import UIKit
 
-var str = "Hello, playground"
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var cacheDic = [Int:Int]()
+        for (index, num) in nums.enumerated() {
+            guard let pairedIndex = cacheDic[target - num] else {
+                cacheDic[num] = index
+                continue
+            }
+            return [pairedIndex,index]
+        }
+        return [-1,-1]
+    }
+}
+
+let solution = Solution()
+solution.twoSum([6,2,5,1], 8)
