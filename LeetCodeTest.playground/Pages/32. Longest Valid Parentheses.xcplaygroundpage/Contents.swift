881d1968055c69285577d6bc25919c4687d098bb
tangyuwei <tangyuwei@myhexin.com>
2018-02-28 10:53:06 +0800

    32题完成

--- LeetCodeTest.playground/Pages/32. Longest Valid Parentheses.xcplaygroundpage/Contents.swift
+++ LeetCodeTest.playground/Pages/32. Longest Valid Parentheses.xcplaygroundpage/Contents.swift
@@ -4,12 +4,27 @@
  For "(()", the longest valid parentheses substring is "()", which has length = 2.
  
  Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
+  思路 1. 用数组记录每个（  堆栈的位置
+  思路 2. 记录（ 与）的个数，当相等时 进行判断 （这需要左右遍历两次）
  */
 
 import Foundation
 
 class Solution {
     func longestValidParentheses(_ s: String) -> Int {
-        return 1
+        var maxNum = 0
+        var stack:[Int] = [-1]
+        for (index, char) in s.enumerated() {
+            if String(char) == "(" {
+                stack.append(index)
+            } else {
+                stack.removeLast()
+                if stack.count == 0 {
+                    stack.append(index)
+                }
+                maxNum = max(maxNum, index - stack.last!)
+            }
+        }
+        return maxNum
     }
 }
---
+++ LeetCodeTest.playground/Pages/33. Search in Rotated Sorted Array.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+//: [Next](@next)
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        
+    }
+}
