b7b431aaae9e386f95e96e358b945daeec00b7c6
tangyuwei <tangyuwei@myhexin.com>
2018-03-08 11:05:37 +0800

    34题完成

--- LeetCodeTest.playground/Pages/34. Search for a Range.xcplaygroundpage/Contents.swift
+++ LeetCodeTest.playground/Pages/34. Search for a Range.xcplaygroundpage/Contents.swift
@@ -11,6 +11,28 @@
  */
 import Foundation
 
-var str = "Hello, playground"
-
-//: [Next](@next)
+class Solution {
+    func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
+        
+        func searchBorder(_ nums: [Int], _ target: Int, _ isleft: Bool) -> Int{
+            var lo = 0
+            var hi = nums.count
+            while (lo < hi) {
+                let mid = (lo + hi) / 2
+                if nums[mid] > target || isleft && target == nums[mid] {
+                    hi = mid
+                } else {
+                    lo = mid + 1
+                }
+            }
+            return lo
+        }
+        
+        let leftIdx = searchBorder(nums, target, true)
+        if leftIdx == nums.count || nums[leftIdx] != target {
+            return [-1, -1]
+        }
+        let rightIdx = searchBorder(nums, target, false)
+        return [leftIdx, rightIdx - 1]
+    }
+}
