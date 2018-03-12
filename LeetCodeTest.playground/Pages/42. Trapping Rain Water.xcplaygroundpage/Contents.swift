d03b06ede372d9e3724e948e00c21289581e7e25
tangyuwei <tangyuwei@myhexin.com>
2018-03-12 09:43:32 +0800

    42 题完成

--- LeetCodeTest.playground/Pages/42. Trapping Rain Water.xcplaygroundpage/Contents.swift
+++ LeetCodeTest.playground/Pages/42. Trapping Rain Water.xcplaygroundpage/Contents.swift
@@ -8,10 +8,21 @@
  The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped. Thanks Marcos for contributing this image!
  */
 
+// 思路 两端开始遍历，每一格子计算
 import Foundation
 
 class Solution {
     func trap(_ height: [Int]) -> Int {
-        return 0
+        var left = 0, right = height.count - 1 , max_left = 0, max_right = 0, ans = 0
+        while left < right {
+            if height[left] < height[right] {
+                height[left] >= max_left ? (max_left = height[left]) : (ans = (max_left - height[left]) + ans)
+                left += 1
+            } else {
+                height[right] >= max_right ? (max_right = height[right]) : (ans = (max_right - height[right]) + ans)
+                right -= 1
+            }
+        }
+        return ans
     }
 }
---
+++ LeetCodeTest.playground/Pages/46. Permutations.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+/*
+ 
+ Given a collection of distinct numbers, return all possible permutations.
+ 
+ For example,
+ [1,2,3] have the following permutations:
+ [
+ [1,2,3],
+ [1,3,2],
+ [2,1,3],
+ [2,3,1],
+ [3,1,2],
+ [3,2,1]
+ ]
+ */
+import Foundation
+
+class Solution {
+    func permute(_ nums: [Int]) -> [[Int]] {
+        return []
+    }
+}
