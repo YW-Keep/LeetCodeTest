c66ae6bfbfbf0b75c12ea780b433a5ec663e96df
tangyuwei <tangyuwei@myhexin.com>
2018-03-20 10:18:14 +0800

    56题 完成

--- LeetCodeTest.playground/Pages/56. Merge Intervals.xcplaygroundpage/Contents.swift
+++ LeetCodeTest.playground/Pages/56. Merge Intervals.xcplaygroundpage/Contents.swift
@@ -8,6 +8,33 @@
 
 import Foundation
 
-var str = "Hello, playground"
-
-//: [Next](@next)
+public class Interval {
+    public var start: Int
+    public var end: Int
+    public init(_ start: Int, _ end: Int) {
+        self.start = start
+        self.end = end
+    }
+}
+ 
+class Solution {
+    func merge(_ intervals: [Interval]) -> [Interval] {
+        let listArray = intervals.sorted { (value1, value2) -> Bool in
+            value1.start < value2.start
+        }
+        var result: [Interval] = []
+        for interval in listArray {
+            guard result.count > 0 else {
+                result.append(interval)
+                continue
+            }
+            let lastInterval = result.last!
+            if lastInterval.end < interval.start {
+                result.append(interval)
+            } else {
+                lastInterval.end = max(lastInterval.end, interval.end)
+            }
+        }
+        return result
+    }
+}
---
+++ LeetCodeTest.playground/Pages/62. Unique Paths.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+/*
+ A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ 
+ The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ 
+ How many possible unique paths are there?
+ */
+
+import Foundation
+
+class Solution {
+    func uniquePaths(_ m: Int, _ n: Int) -> Int {
+        return 1
+    }
+}
