3b96444e08e2d1950a1f28baaea4d29f0277a63f
tangyuwei <tangyuwei@myhexin.com>
2018-03-26 15:04:50 +0800

    72题完成

--- LeetCodeTest.playground/Pages/72. Edit Distance.xcplaygroundpage/Contents.swift
+++ LeetCodeTest.playground/Pages/72. Edit Distance.xcplaygroundpage/Contents.swift
@@ -9,6 +9,40 @@
  */
 import Foundation
 
-var str = "Hello, playground"
-
-//: [Next](@next)
+class Solution {
+    func minDistance(_ word1: String, _ word2: String) -> Int {
+        let num1 = Int(word1.count)
+        let num2 = Int(word2.count)
+        
+        guard num1 > 0 else {
+            return num2
+        }
+        
+        guard num2 > 0 else {
+            return num1
+        }
+        
+        var record: [[Int]] = [[Int]](repeating: [Int](repeating: -1, count: num2 + 1), count: num1 + 1)
+        for indx in 0...num1 {
+            record[indx][0] = indx
+        }
+        
+        for indx in 0...num2 {
+            record[0][indx] = indx
+        }
+        
+        for row in 1...num1 {
+            for index in 1...num2 {
+                let char1 = word1[word1.index(word1.startIndex, offsetBy: row - 1)]
+                let char2 = word2[word2.index(word2.startIndex, offsetBy: index - 1)]
+                if char1 == char2 {
+                    record[row][index] = record[row - 1][index - 1]
+                } else {
+                    record[row][index] = min(record[row - 1][index - 1] + 1, record[row][index - 1] + 1,record[row - 1][index] + 1)
+                }
+            }
+        }
+        
+        return record[num1][num2]
+    }
+}
---
+++ LeetCodeTest.playground/Pages/75. Sort Colors.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+/*
+ 
+ Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+ 
+ Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+ 
+ Note:
+ You are not suppose to use the library's sort function for this problem.
+ 
+ click to show follow up.
+ 
+
+ */
+import Foundation
+
+var str = "Hello, playground"
+
+var a = str.index(str.startIndex, offsetBy: 1)
+var m = str[str.index(str.startIndex, offsetBy: 1)]
+
+var text: [Int] = [Int](repeating: -1, count: 5)
+text[2] = 1
+
