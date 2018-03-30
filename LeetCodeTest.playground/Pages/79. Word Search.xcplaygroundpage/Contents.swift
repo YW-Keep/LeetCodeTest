b64b0c6b2aba405c7661588dfef98ce68497df17
tangyuwei <tangyuwei@myhexin.com>
2018-03-30 13:53:50 +0800

    79题完成

--- LeetCodeTest.playground/Pages/79. Word Search.xcplaygroundpage/Contents.swift
+++ LeetCodeTest.playground/Pages/79. Word Search.xcplaygroundpage/Contents.swift
@@ -18,6 +18,65 @@
 
 import Foundation
 
-var str = "Hello, playground"
-
-//: [Next](@next)
+class Solution {
+    func exist(_ board: [[Character]], _ word: String) -> Bool {
+        
+        guard word.count > 0 else {
+            return true
+        }
+        
+        let head = word[word.index(word.startIndex, offsetBy: 0)]
+        for (i, array) in board.enumerated() {
+            for (j,char) in array.enumerated() {
+                if  char == head {
+                    // 回溯算法计算
+                    var newWord = word
+                    newWord.remove(at: newWord.startIndex)
+                    let record = String(i) + "," + String(j)
+                    if self.findString(board, newWord, (i, j),[record]) {
+                        return true
+                    }
+                }
+            }
+        }
+        return false
+    }
+    
+    func findString(_ board: [[Character]], _ word: String, _ indx:(Int,Int), _ records:[String]) -> Bool {
+        
+        guard word.count > 0 else {
+            return true
+        }
+        
+        func judge(_ indx: (Int,Int)) -> Bool {
+            let record = String(indx.0) + "," + String(indx.1)
+            guard !records.contains(record) else
+            {
+                return false
+            }
+            let head = word[word.index(word.startIndex, offsetBy: 0)]
+            if board[indx.0][indx.1] == head {
+                var newWord = word
+                newWord.remove(at: newWord.startIndex)
+                if (self.findString(board, newWord, indx, records + [record])) {
+                    return true
+                }
+            }
+            return false
+        }
+        
+        if 0 <= (indx.0 - 1) && judge((indx.0 - 1, indx.1)) {
+            return true
+        }
+        if 0 <= (indx.1 - 1) && judge((indx.0, indx.1 - 1)) {
+            return true
+        }
+        if(indx.0 + 1) < board.count && judge((indx.0 + 1, indx.1)) {
+            return true
+        }
+        if(indx.1 + 1) < board[indx.0].count && judge((indx.0, indx.1 + 1)) {
+            return true
+        }
+        return false
+    }
+}
---
+++ LeetCodeTest.playground/Pages/84. Largest Rectangle in Histogram.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+//: [Previous](@previous)
+
+import Foundation
+
+class Solution {
+    func largestRectangleArea(_ heights: [Int]) -> Int {
+        return 0
+    }
+}
