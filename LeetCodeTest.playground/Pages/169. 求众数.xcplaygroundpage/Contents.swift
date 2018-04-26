2e835cc8107ea834b4497b09e31897e2c006347f
tangyuwei <tangyuwei@myhexin.com>
2018-04-26 09:23:42 +0800

    169完成

--- LeetCodeTest.playground/Pages/169. 求众数.xcplaygroundpage/Contents.swift
+++ LeetCodeTest.playground/Pages/169. 求众数.xcplaygroundpage/Contents.swift
@@ -4,10 +4,13 @@
  
  你可以假设数组是非空的，并且数组中的众数永远存在。
  */
+// 因为超过半数 所以排序后中位数 肯定是它  只要排序就好了
 import Foundation
 
 class Solution {
     func majorityElement(_ nums: [Int]) -> Int {
-        return 0
+        var newNums = nums
+        newNums.sort()
+        return newNums[nums.count / 2]
     }
 }
