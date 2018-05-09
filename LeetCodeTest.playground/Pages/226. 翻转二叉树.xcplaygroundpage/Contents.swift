6fbcddfd66988f984011fe770184cfe2f58a807a
tangyuwei <tangyuwei@myhexin.com>
2018-05-09 10:53:43 +0800

    226题完成

--- LeetCodeTest.playground/Pages/226. 翻转二叉树.xcplaygroundpage/Contents.swift
+++ LeetCodeTest.playground/Pages/226. 翻转二叉树.xcplaygroundpage/Contents.swift
@@ -1,7 +1,48 @@
-//: [Previous](@previous)
+/*
+ 翻转一棵二叉树。
+ 
+ 示例：
+ 
+ 输入：
+ 
+ 4
+ /   \
+ 2     7
+ / \   / \
+ 1   3 6   9
+ 输出：
+ 
+ 4
+ /   \
+ 7     2
+ / \   / \
+ 9   6 3   1
+ 备注:
+ 这个问题是受到 Max Howell 的 原问题 启发的 ：
+ 
+ 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+ */
 
 import Foundation
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
 
-var str = "Hello, playground"
-
-//: [Next](@next)
+class Solution {
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        guard root != nil else {
+            return root
+        }
+        let left = invertTree(root?.left)
+        root?.left = invertTree(root?.right)
+        root?.right = left
+        return root
+    }
+}
