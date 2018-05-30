8194a1138ecf77b9f65d2d9c9711ab1b5704c565
tangyuwei <tangyuwei@myhexin.com>
2018-05-30 10:23:29 +0800

    406题完成

--- LeetCodeTest.playground/Pages/406. 根据身高重建队列.xcplaygroundpage/Contents.swift
+++ LeetCodeTest.playground/Pages/406. 根据身高重建队列.xcplaygroundpage/Contents.swift
@@ -17,6 +17,28 @@
 
 import Foundation
 
-var str = "Hello, playground"
-
-//: [Next](@next)
+// 思路： 首先想到的肯定是排序 k 是在这个人前面升高大于或等于这个人的个数 而h是身高。 那么如果后面的数都小于前面的数，按理论上操纵后面的数对前面的数的K 不会有影响。 所以按照这个思路，我们就可以先对其进行排序了
+// 另外出题的人 其实对swift 不了解 因为我们知道swift有一种数据结构元祖，这里用元祖可能会好一些
+class Solution {
+    func reconstructQueue(_ people: [[Int]]) -> [[Int]] {
+        
+        var newPeople = people
+        // 利用系统方法排序
+        newPeople.sort { (first, second) -> Bool in
+            if first[0] == second[0] {
+                return first[1] < second[1]
+            } else {
+                return first[0] > second[0]
+            }
+        }
+        var reslut: [[Int]] = []
+        for person in newPeople {
+            if person[1] >= reslut.count {
+                reslut.append(person)
+            } else {
+                reslut.insert(person, at: person[1])
+            }
+        }
+        return reslut
+    }
+}
---
+++ LeetCodeTest.playground/Pages/417.平分成两个和相同的子集.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+/*
+ 给定一个只包含正整数的非空数组，如果数组可以被划分为两个子集，那么这两个子集中的元素之和是相等的。
+ 
+ 注意:
+ 每个数组元素都不会超过100。
+ 数组大小不会超过200。
+ Example 1:
+ 
+ Input: [1, 5, 11, 5]
+ 
+ Output: true
+ 
+ Explanation: The array can be partitioned as [1, 5, 5] and [11].
+ Example 2:
+ 
+ Input: [1, 2, 3, 5]
+ 
+ Output: false
+ 
+ Explanation: The array cannot be partitioned into equal sum subsets.
+ */
+
+import Foundation
+
+class Solution {
+    func canPartition(_ nums: [Int]) -> Bool {
+        return true
+    }
+}
