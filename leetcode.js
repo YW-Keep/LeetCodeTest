ddd8ac54b908196c496a7c0b6a40213e0e917704
tangyuwei <tangyuwei@myhexin.com>
2018-07-10 13:19:26 +0800

    更新js算法

--- leetcode.js
+++ leetcode.js
@@ -74,3 +74,50 @@ var lengthOfLongestSubstring = function(s) {
     }
     return reslut;
 };
+
+//4. 两个排序数组的中位数   排序数组，其实就是取数  二分法求值
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+
+    let medianNum = Math.floor((nums1.length + nums2.length)/2);
+    if ((nums1.length + nums2.length)%2 > 0) {
+        return findValueInSortedArray(medianNum, nums1, nums2);
+    } else {
+        let lowerNum = findValueInSortedArray(medianNum - 1, nums1, nums2);
+        let higherNum = findValueInSortedArray(medianNum, nums1, nums2);
+        return (lowerNum + higherNum)/2;
+    }
+};
+
+/**
+ * @param {number}   k
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findValueInSortedArray = function(k, num1, num2) {
+
+    if (num1.length > num2.length) {
+        return findValueInSortedArray(k, num2, num1);
+    }
+
+    if (num1.length == 0) {
+        return num2[k];
+    }
+
+    if(k <= 0) {
+        return Math.min(num1[0],num2[0]);
+    }
+
+    let index1 = Math.min(Math.floor((k -1)/2),num1.length - 1);
+    let index2 = Math.min(Math.floor((k -1)/2),num2.length - 1);
+    if (num1[index1] < num2[index2]) {
+        return findValueInSortedArray(k - index1 - 1, num1.slice(index1 + 1, num1.length) , num2);
+    } else {
+        return findValueInSortedArray(k - index2 - 1, num1, num2.slice(index2 + 1, num2.length));
+    }
+}
