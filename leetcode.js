def2a9867a4ed33beaf52a4216a18dac52e4fdeb
tangyuwei <tangyuwei@myhexin.com>
2018-08-01 10:52:21 +0800

    更新JS

--- leetcode.js
+++ leetcode.js
@@ -586,3 +586,28 @@ var searchRange = function(nums, target) {
     return [leftR, rightR];
     
 };
+
+// 35. 搜索插入位置 又是一题二分法的题目
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    var left = 0 , right = nums.length - 1 , mid = 0;
+    while (left < right) {
+        mid = Math.floor((left + right)/2);
+        if(nums[mid] == target) {
+            return mid;
+        } else if(nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }    
+    if(nums[left] <  target ) {
+        left++;
+    }
+    return left;
+};
