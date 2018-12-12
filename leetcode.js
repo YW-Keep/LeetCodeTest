97d8bdff3f4d6eaf82f2cab1c9b0de8320dca46d
tangyuwei <tangyuwei@myhexin.com>
2018-12-12 14:13:31 +0800

    更新jsC

--- leetcode.js
+++ leetcode.js
@@ -4020,6 +4020,23 @@ var numJewelsInStones = function(J, S) {
     return count;
 };
 
+// 779. 第K个语法符号  递归  后面一个数肯定是由前面一个数推断过来的 所以不需要知道完整的数列
+/**
+ * @param {number} N
+ * @param {number} K
+ * @return {number}
+ */
+var kthGrammar = function(N, K) {
+    if(N == 1) {
+        return 0;
+    }
+    if(K%2 == 0) {
+        return kthGrammar(N-1,K/2) == 0 ? 1 : 0;
+    } else {
+        return kthGrammar(N-1,(K + 1)/2) == 0 ? 0 : 1;
+    }
+ };
+
 // 788.旋转数字 简单的逻辑思考
 /**
  * @param {number} N
