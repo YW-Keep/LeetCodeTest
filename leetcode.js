34176e984e73c3921bcfb771d6b7de4f45828080
tangyuwei <tangyuwei@myhexin.com>
2019-04-28 10:20:50 +0800

    更新js

--- leetcode.js
+++ leetcode.js
@@ -7675,6 +7675,17 @@ var removeOuterParentheses = function(S) {
     }
     return result;
 };
+// 1025. 除数博弈 逻辑题 因为先手为偶数的话，先手只需要让自己每步都保持偶数，那么他可以通过让对手得到的数为奇数，比如偶数-1就是奇数了，对手拿到奇数，那么能整除的只有奇数，奇数-奇数又回到了偶数，最后先手一定会得到最小的偶数2，然后-1让对手得到1，对手无解，必胜。
+
+/**
+ * @param {number} N
+ * @return {boolean}
+ */
+var divisorGame = function(N) {
+    return N%2 == 0 ? true : false;
+};
+
+
 
 // 1029.两地调度 贪心算法
 /**
