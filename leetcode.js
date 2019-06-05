30b89fcdd4e97cdb689a4ffe9e662d8409015a84
tangyuwei <tangyuwei@myhexin.com>
2019-06-05 14:27:02 +0800

    更新js

--- leetcode.js
+++ leetcode.js
@@ -5429,6 +5429,25 @@ var findShortestSubArray = function(nums) {
     return min;
   };
 
+  // 700. 二叉搜索树中的搜索 递归
+  /**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    if(root == null) {
+        return null;
+    }
+    if(root.val == val) {
+        return root;
+    } else if(root.val > val) {
+        return searchBST(root.left,val);
+    } else {
+        return searchBST(root.right,val);
+    }
+};
+
 // 704. 二分查找  如题
 /**
  * @param {number[]} nums
