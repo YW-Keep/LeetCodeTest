2d80b999973de691325e2b9922bbe99334bbcf4f
tangyuwei <tangyuwei@myhexin.com>
2019-06-10 09:23:33 +0800

    更新js

--- leetcode.js
+++ leetcode.js
@@ -1482,7 +1482,25 @@ var addBinary = function(a, b) {
     return result;
 };
 
-addBinary("1010","1011");
+// 69. x 的平方根  二分查找
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let l = 0;
+    let r = Math.floor(x/2) +1;
+    while (l < r) {
+        let mid = Math.floor((r+l)/2);
+        if(mid*mid > x) {
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return l*l > x ? l - 1 : l;
+    
+};
 
 // 72. 编辑距离  其实是动态规划问题 但是要考虑到2维动态规划 ，需要一个二维数组
 
