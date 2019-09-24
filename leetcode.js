6fbd12e57779a79e990d249da458ec94b6ff6818
tangyuwei <tangyuwei@myhexin.com>
2019-09-24 10:28:19 +0800

    更新js

--- leetcode.js
+++ leetcode.js
@@ -6647,6 +6647,17 @@ var constructArray = function(n, k) {
     return arr
 };
 
+// 669.修剪二叉搜索树  递归
+var trimBST = function(root, L, R) {
+    if(root == null) return root;
+    if(root.val > R) return trimBST(root.left,L,R);
+    if(root.val < L) return trimBST(root.right,L,R);
+
+    root.left = trimBST(root.left,L,R);
+    root.right = trimBST(root.right,L,R);
+    return root;
+};
+
 // 670.最大交换  排序对比
 /**
  * @param {number} num
