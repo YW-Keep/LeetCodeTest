cae5b0047743b23033d68fee6eb8ff257cb814c0
tangyuwei <tangyuwei@myhexin.com>
2020-02-25 09:48:45 +0800

    更新js

--- 程序员面试金典 /codeTest.js
+++ 程序员面试金典 /codeTest.js
@@ -199,3 +199,13 @@ var setZeroes = function(matrix) {
         }
     }
 };
+
+// 面试题 01.09. 字符串轮转 基础题 双倍子串
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var isFlipedString = function(s1, s2) {
+    return s1.length == s2.length &&  (s1+s1).indexOf(s2) != -1;
+};
