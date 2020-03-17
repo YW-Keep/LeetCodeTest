cfd45f5c582351e01e541521a2bfbe850b9f7e62
tangyuwei <tangyuwei@myhexin.com>
2020-03-17 09:57:17 +0800

    更新js

--- 程序员面试金典 /codeTest.js
+++ 程序员面试金典 /codeTest.js
@@ -936,3 +936,30 @@ var inorderSuccessor = function(root, p) {
     }
     return null
 };
+
+//  面试题 04.08. 首个共同祖先  递归
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if(!root) { return null}
+    if(root == p || root == q) {
+        return root
+    }
+    let left = lowestCommonAncestor(root.left,p,q)
+    let right = lowestCommonAncestor(root.right,p,q)
+    if(left && right) {
+        return root
+    }
+    return left ? left : right;
+};
