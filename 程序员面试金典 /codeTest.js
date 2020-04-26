9859a6cf79ef02f064cb6c7ba79c3b53024cfa90
tangyuwei <tangyuwei@myhexin.com>
2020-04-26 09:56:51 +0800

    更新js

--- 程序员面试金典 /codeTest.js
+++ 程序员面试金典 /codeTest.js
@@ -1903,3 +1903,48 @@ var numberToWords = function(num) {
     return res
 };
 
+// 面试题 16.10. 生存人数 双指针
+/**
+ * @param {number[]} birth
+ * @param {number[]} death
+ * @return {number}
+ */
+var maxAliveYear = function(birth, death) {
+    birth.sort((a,b)=> a - b)
+    death.sort((a,b) => a - b)
+    let i = 0,j = 0, maxNum =0,res = 0,length = birth.length,num = 0
+    while(i < length && j < length) {
+        if(birth[i] <= death[j]) {
+            num++
+            if(num > maxNum) {
+                maxNum = num
+                res = birth[i]
+            }
+            i++
+        } else {
+            num--
+            j++
+        }
+    }
+    return res
+};
+
+// 面试题 16.11. 跳水板 基础逻辑
+/**
+ * @param {number} shorter
+ * @param {number} longer
+ * @param {number} k
+ * @return {number[]}
+ */
+var divingBoard = function(shorter, longer, k) {
+    if(k == 0) { return []}
+    if(shorter == longer) {
+        return [shorter*k]
+    }
+    let result = []
+    for (let i = 0; i <= k; i++) {
+        let num =  shorter*(k - i) + longer*i
+        result.push(num)
+    }
+    return result
+};
