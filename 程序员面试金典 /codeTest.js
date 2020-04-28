bebfb16529195b0d0b79c05bbbede95d3c00ad4f
tangyuwei <tangyuwei@myhexin.com>
2020-04-28 09:18:28 +0800

    更新js

--- 程序员面试金典 /codeTest.js
+++ 程序员面试金典 /codeTest.js
@@ -1977,3 +1977,28 @@ var masterMind = function(solution, guess) {
     }
     return [num,res-num];
 };
+
+// 面试题 16.16. 部分排序  前后两次遍历找到需要重排的点
+/**
+ * @param {number[]} array
+ * @return {number[]}
+ */
+var subSort = function(array) {
+    let  r = -1, max = Number.MIN_SAFE_INTEGER
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] >= max) {
+            max = array[i];
+        } else {
+            r = i;
+        }
+    }
+    let l = -1, min =  Number.MAX_SAFE_INTEGER
+    for (let i = array.length -1; i >= 0; i--) {
+        if(array[i] <= min) {
+            min = array[i]
+        } else {
+            l = i
+        }
+    }
+    return [l,r];
+};
