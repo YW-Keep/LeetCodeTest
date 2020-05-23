8e23d307d1729fb6c7a4a5d1627311077e87cd70
tangyuwei <tangyuwei@myhexin.com>
2020-05-23 13:34:40 +0800

    更新js

--- 剑指offer/code.js
+++ 剑指offer/code.js
@@ -17,3 +17,23 @@ var findRepeatNumber = function(nums) {
     }
     return
 };
+// 面试题04. 二维数组中的查找  角上查找法 
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var findNumberIn2DArray = function(matrix, target) {
+    if(matrix.length == 0) {return false}
+    let i = 0,j = matrix[0].length -1;
+    while(i < matrix.length && j >= 0) {
+        if(matrix[i][j] == target) {
+            return true
+        } else if(matrix[i][j] > target) {
+            j--
+        } else {
+            i++
+        }
+    }
+    return false
+};
