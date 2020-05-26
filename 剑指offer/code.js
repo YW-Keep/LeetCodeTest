6e5ba773342fba4e1ce7c41e37e18fe25a70b646
tangyuwei <tangyuwei@myhexin.com>
2020-05-26 10:11:49 +0800

    更新js

--- 剑指offer/code.js
+++ 剑指offer/code.js
@@ -59,3 +59,28 @@ var reversePrint = function(head) {
     }
     return res
 };
+
+// 面试题07. 重建二叉树  递归
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+    if(preorder.length == 0) {
+        return null
+    }
+    let node = new TreeNode(preorder[0])
+    let i = 0;
+    while(inorder[i] != preorder[0]) {
+        i++;
+    }
+    
+    node.left = buildTree(preorder.slice(1,i+1),inorder.slice(0,i))
+    node.right = buildTree(preorder.slice(i+1), inorder.slice(i+1))
+    return node
+};
