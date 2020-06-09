5aa7d33e2101da34798ad19661cf889c3f8d9ada
tangyuwei <tangyuwei@myhexin.com>
2020-06-09 10:12:22 +0800

    更新js

--- 剑指offer/code.js
+++ 剑指offer/code.js
@@ -376,3 +376,39 @@ var reverseList = function(head) {
     }
     return before;
 };
+
+//  面试题25. 合并两个排序的链表 遍历
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    let head  = new ListNode(1);
+    let next = head
+    while(l1 && l2) {
+        if(l1.val > l2.val) {
+            next.next = l2
+            l2  = l2.next
+            next = next.next
+        } else {
+            next.next = l1
+            l1  = l1.next
+            next = next.next
+        }
+    }
+    if(l1) {
+        next.next = l1
+    }
+    if(l2) {
+        next.next =l2
+    }
+    return head.next
+};
