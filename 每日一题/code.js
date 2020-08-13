1c8ca58eb25c33007e66fcad8b4fb67a6edf819b
tangyuwei <tangyuwei@myhexin.com>
2020-08-13 09:48:35 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -526,3 +526,30 @@ var cloneGraph = function(node) {
   }
   return newNode; 
 };
+// 43.字符串相乘 每个相乘再累加
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+  if(num1 == '0' || num2 == '0') {
+    return '0'
+  }
+  let m = num1.length,n = num2.length,arr = new Array(m +n).fill(0)
+  for (let i = m -1; i >= 0; i--) {
+    let x = Number.parseInt(num1[i])
+    for (let j = n -1; j >= 0; j --) {
+      let y = Number.parseInt(num2[j])
+      arr[i+j+1] += x*y
+    }
+  }
+  for (let i = m +n -1; i > 0; i--) {
+    arr[i -1] += Math.floor(arr[i]/10)
+    arr[i] %=10 
+  }
+  if(arr[0] == 0) {
+    arr.shift()
+  } 
+  return arr.join('')
+};
