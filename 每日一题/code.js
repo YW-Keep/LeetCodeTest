c80b4aedfb965f808f978050389583d9e798ecce
tangyuwei <tangyuwei@myhexin.com>
2020-08-19 11:11:18 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -714,28 +714,23 @@ var countSubstrings = function(s) {
   let n = s.length;
   let t = ['$', '#'];
   for (let i = 0; i < n; ++i) {
-      t.push(s.charAt(i));
+      t.push(s[i]);
       t.push('#');
   }
   n = t.length;
   t.push('!');
-  t = t.join('');
 
-  const f = new Array(n);
+  let f = new Array(n).fill(0);
   let iMax = 0, rMax = 0, ans = 0;
-  for (let i = 1; i < n; ++i) {
-      // 初始化 f[i]
-      f[i] = i <= rMax ? Math.min(rMax - i + 1, f[2 * iMax - i]) : 1;
-      // 中心拓展
-      while (t.charAt(i + f[i]) == t.charAt(i - f[i])) {
-          ++f[i];
+  for (let i = 2; i < n; i++) {
+      f[i] = i < rMax ? Math.min(rMax - i + 1, f[2 * iMax - i]) : 1;
+      while (t[i + f[i]] == t[i - f[i]]) {
+          f[i]++;
       }
-      // 动态维护 iMax 和 rMax
       if (i + f[i] - 1 > rMax) {
           iMax = i;
           rMax = i + f[i] - 1;
       }
-      // 统计答案, 当前贡献为 (f[i] - 1) / 2 上取整
       ans += Math.floor(f[i] / 2);
   }
 
