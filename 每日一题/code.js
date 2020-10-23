cdd146623c23d22a4602290bfada1d815cd62515
tangyuwei <tangyuwei@myhexin.com>
2020-10-23 09:17:49 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -2234,3 +2234,29 @@ var partitionLabels = function(S) {
   }
   return res;
 };
+
+// 234. 回文链表  双指针
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+  const vals = [];
+  while (head !== null) {
+      vals.push(head.val);
+      head = head.next;
+  }
+  for (let i = 0, j = vals.length - 1; i < j; ++i, --j) {
+      if (vals[i] !== vals[j]) {
+          return false;
+      }
+  }
+  return true;
+};
