83c2357dfeaa4f00a6858ce1a41235508184b9ab
tangyuwei <tangyuwei@myhexin.com>
2020-11-04 09:35:39 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -2531,3 +2531,32 @@ var validMountainArray = function(A) {
   }
   return i == (N-1)
 };
+// 57. 插入区间 逻辑题
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function(intervals, newInterval) {
+  const res = [];
+  let i = 0;
+  const len = intervals.length;
+
+  while (i < len && intervals[i][1] < newInterval[0]) { 
+    res.push([intervals[i][0], intervals[i][1]]);
+    i++;
+  }
+  while (i < len && intervals[i][0] <= newInterval[1]) { 
+    newInterval[0] = Math.min(intervals[i][0], newInterval[0]); 
+    newInterval[1] = Math.max(intervals[i][1], newInterval[1]); 
+    i++;
+  }
+  res.push([newInterval[0], newInterval[1]]); 
+
+  while (i < len) {               
+    res.push([intervals[i][0], intervals[i][1]]);
+    i++;
+  }
+  
+  return res;
+}
