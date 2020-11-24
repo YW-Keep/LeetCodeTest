872c6763522d9c0bfb88d431188ab379c52706bb
tangyuwei <tangyuwei@myhexin.com>
2020-11-24 09:47:39 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -3075,3 +3075,22 @@ var findMinArrowShots = function(points) {
   }
   return ans;
 };
+
+// 222. 完全二叉树的节点个数  递归
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function(root) {
+  if(!root) {
+    return 0
+  }
+  return countNodes(root.left) + countNodes(root.right) + 1
+};
