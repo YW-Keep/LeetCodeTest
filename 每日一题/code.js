901609239f7b84c3e0a4f610e22f352a4769b8f7
tangyuwei <tangyuwei@myhexin.com>
2020-11-25 09:20:46 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -3094,3 +3094,32 @@ var countNodes = function(root) {
   }
   return countNodes(root.left) + countNodes(root.right) + 1
 };
+
+// 1370. 上升下降字符串 基础逻辑来回遍历
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var sortString = function(s) {
+  const num = new Array(26).fill(0);
+  for (let ch of s) {
+      num[ch.charCodeAt() - 'a'.charCodeAt()]++;
+  }
+
+  let ret = '';
+  while (ret.length < s.length) {
+      for (let i = 0; i < 26; i++) {
+          if (num[i]) {
+              ret += String.fromCharCode(i + 'a'.charCodeAt());
+              num[i]--;
+          }
+      }
+      for (let i = 25; i >= 0; i--) {
+          if (num[i]) {
+              ret += String.fromCharCode(i + 'a'.charCodeAt());
+              num[i]--;
+          }
+      }
+  }
+  return ret;
+};
