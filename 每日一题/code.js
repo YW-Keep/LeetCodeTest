57a510c15c84eb677a4ab8cafaa933bdd934eb0e
tangyuwei <tangyuwei@myhexin.com>
2020-11-27 09:49:52 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -3141,3 +3141,18 @@ var maximumGap = function(nums) {
     }
     return max;
 };
+
+// 454. 四数相加 II 分两组计算 
+var fourSumCount = function(A, B, C, D) {
+  const countAB = new Map();
+  A.forEach(u => B.forEach(v => countAB.set(u + v, (countAB.get(u + v) || 0) + 1)));
+  let ans = 0; 
+  for (let u of C) {
+      for (let v of D) {
+          if (countAB.has(-u - v)) {
+              ans += countAB.get(-u - v);
+          }
+      }
+  }
+  return ans;
+};
