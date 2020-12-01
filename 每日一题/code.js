a9b1fe35b92cd95441179cbb23b8c8c2ad177062
tangyuwei <tangyuwei@myhexin.com>
2020-12-01 09:23:43 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -3237,3 +3237,28 @@ var reorganizeString = function(S) {
       return String.fromCharCode(c)
     }
 };
+
+// 34. 在排序数组中查找元素的第一个和最后一个位置 二分查找
+var searchRange = function(nums, target) {
+  let ans = [-1, -1];
+  const leftIdx = binarySearch(nums, target, true);
+  const rightIdx = binarySearch(nums, target, false) - 1;
+  if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] === target && nums[rightIdx] === target) {
+      ans = [leftIdx, rightIdx];
+  } 
+  return ans;
+
+  function binarySearch (nums, target, lower)  {
+    let left = 0, right = nums.length - 1, ans = nums.length;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] > target || (lower && nums[mid] >= target)) {
+            right = mid - 1;
+            ans = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return ans;
+  }
+};
