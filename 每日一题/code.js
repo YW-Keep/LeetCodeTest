b7788b5282d4452664afe66926012dde9a57b4ab
tangyuwei <tangyuwei@myhexin.com>
2020-12-02 09:59:22 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -3262,3 +3262,34 @@ var searchRange = function(nums, target) {
     return ans;
   }
 };
+
+// 999. 可以被一步捕获的棋子数 基础题 
+var numRookCaptures = function(board) {
+  let cnt = 0, st = 0, ed = 0;
+  const dx = [0, 1, 0, -1];
+  const dy = [1, 0, -1, 0];
+
+  for (let i = 0; i < 8; ++i) {
+      for (let j = 0; j < 8; ++j) {
+          if (board[i][j] == 'R') {
+              st = i;
+              ed = j;
+              break;
+          }
+      }
+  }
+  for (let i = 0; i < 4; ++i) {
+      for (let step = 0;; ++step) {
+          const tx = st + step * dx[i];
+          const ty = ed + step * dy[i];
+          if (tx < 0 || tx >= 8 || ty < 0 || ty >= 8 || board[tx][ty] == 'B') {
+              break;
+          }
+          if (board[tx][ty] == 'p') {
+              cnt++;
+              break;
+          }
+      }
+  }
+  return cnt;
+};
