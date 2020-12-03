faa0c1463380933dd22aff1f5012fc7e5254186a
tangyuwei <tangyuwei@myhexin.com>
2020-12-03 09:47:54 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -3293,3 +3293,24 @@ var numRookCaptures = function(board) {
   }
   return cnt;
 };
+// 204. 计数质数 基础题
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+  const isPrime = new Array(n).fill(1);
+  const primes = [];
+  for (let i = 2; i < n; ++i) {
+      if (isPrime[i]) {
+          primes.push(i);
+      }
+      for (let j = 0; j < primes.length && i * primes[j] < n; ++j) {
+          isPrime[i * primes[j]] = 0;
+          if (i % primes[j] === 0) {
+              break;
+          }
+      }
+  }
+  return primes.length;
+};
