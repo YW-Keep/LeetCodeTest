0a96da74deb7b672403d637588c0a1794b56b3bf
tangyuwei <tangyuwei@myhexin.com>
2020-12-15 09:42:07 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -3595,3 +3595,26 @@ var wiggleMaxLength = function(nums) {
   }
   return Math.max(up[n -1],down[n-1])
 };
+
+// 738. 单调递增的数字  贪心算法
+/**
+ * @param {number} N
+ * @return {number}
+ */
+var monotoneIncreasingDigits = function(N) {
+  const strN = N.toString().split('').map(v => +v);
+  let i = 1;
+  while (i < strN.length && strN[i - 1] <= strN[i]) {
+      i += 1;
+  }
+  if (i < strN.length) {
+      while (i > 0 && strN[i - 1] > strN[i]) {
+          strN[i - 1] -= 1;
+          i -= 1;
+      }
+      for (i += 1; i < strN.length; ++i) {
+          strN[i] = 9;
+      }
+  }
+  return parseInt(strN.join(''));
+};
