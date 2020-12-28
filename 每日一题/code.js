863fa57976c2b8d748f65b9dee6cb471467fa212
tang <tangyuwei@myhexin.com>
2020-12-28 10:12:54 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -3868,3 +3868,56 @@ var replaceElements = function(arr) {
   }
   return res;
 };
+
+//  205. 同构字符串 双map 
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+  const s2t = {};
+  const t2s = {};
+  const len = s.length;
+  for (let i = 0; i < len; ++i) {
+      const x = s[i], y = t[i];
+      if ((s2t[x] && s2t[x] !== y) || (t2s[y] && t2s[y] !== x)) {
+          return false;
+      }
+      s2t[x] = y;
+      t2s[y] = x;
+  }
+  return true;
+};
+
+// 188. 买卖股票的最佳时机 IV  动态规划
+/**
+ * @param {number} k
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(k, prices) {
+  if (!prices.length) {
+      return 0;
+  }
+
+  const n = prices.length;
+  k = Math.min(k, Math.floor(n / 2));
+  const buy = new Array(k + 1).fill(0);
+  const sell = new Array(k + 1).fill(0);
+
+  [buy[0], sell[0]] = [-prices[0], 0]
+  for (let i = 1; i < k + 1; ++i) {
+      buy[i] = sell[i] = -Number.MAX_VALUE;
+  }
+
+  for (let i = 1; i < n; ++i) {
+      buy[0] = Math.max(buy[0], sell[0] - prices[i]);
+      for (let j = 1; j < k + 1; ++j) {
+          buy[j] = Math.max(buy[j], sell[j] - prices[i]);
+          sell[j] = Math.max(sell[j], buy[j - 1] + prices[i]); 
+      }
+  }
+
+  return Math.max(...sell)
+};
