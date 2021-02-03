34b99403d498a88c358b985dbf43aa0eb4dfa251
tang <tangyuwei@myhexin.com>
2021-02-03 09:43:18 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -4385,3 +4385,54 @@ var characterReplacement = function(s, k) {
   }
   return right - left;
 };
+
+// 480. 滑动窗口中位数  二分查找
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var medianSlidingWindow = function (nums, k) {
+  let isOdd = k % 2;
+  let arr = [];
+  let res = [];
+  let add = function (num) {
+    let left = 0;
+    let right = arr.length;
+    while (left < right) {
+      let mid = Math.floor((left + right) / 2);
+      if (arr[mid] < num) {
+        left = mid + 1
+      } else {
+        right = mid;
+      }
+    }
+    arr.splice(left, 0, num);
+  }
+  let remove = function (num) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+      let mid = Math.floor((left + right) / 2);
+      if (arr[mid] < num) {
+        left = mid + 1
+      } else {
+        right = mid;
+      }
+    }
+    arr.splice(left, 1);
+  }
+  for (let i = 0; i < k - 1; ++i) {
+    add(nums[i])
+  }
+  for (let i = k - 1, len = nums.length; i < len; ++i) {
+    add(nums[i])
+    if (isOdd) {
+      res.push(arr[(k - 1) / 2])
+    } else {
+      res.push((arr[k / 2] + arr[k / 2 - 1]) / 2)
+    }
+    remove(nums[i - k + 1])
+  }
+  return res;
+};
