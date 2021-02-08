f5a038a4f5bc0ca6593fc853ed26654076b12128
tang <tangyuwei@myhexin.com>
2021-02-08 10:19:25 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -4531,3 +4531,34 @@ var maxScore = function(cardPoints, k) {
   }
   return sum - minSum
 };
+
+// 978. 最长湍流子数组 双指针
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maxTurbulenceSize = function(arr) {
+  const n = arr.length;
+  let ret = 1;
+  let left = 0, right = 0;
+
+  while (right < n - 1) {
+      if (left == right) {
+          if (arr[left] == arr[left + 1]) {
+              left++;
+          }
+          right++;
+      } else {
+          if (arr[right - 1] < arr[right] && arr[right] > arr[right + 1]) {
+              right++;
+          } else if (arr[right - 1] > arr[right] && arr[right] < arr[right + 1]) {
+              right++;
+          } else {
+              left = right;
+          }
+      }
+      ret = Math.max(ret, right - left + 1);
+  }
+  return ret;
+};
