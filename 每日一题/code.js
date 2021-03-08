1e916e43ac43d953f3c2335d674532709c523e81
tang <tangyuwei@myhexin.com>
2021-03-08 10:01:53 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -5027,3 +5027,86 @@ MyQueue.prototype.in2out = function() {
       this.outStack.push(this.inStack.pop());
   }
 }
+
+// 503. 下一个更大元素 II 单调栈
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var nextGreaterElements = function(nums) {
+  const n = nums.length;
+  const ret = new Array(n).fill(-1);
+  const stk = [];
+  for (let i = 0; i < n * 2 - 1; i++) {
+      while (stk.length && nums[stk[stk.length - 1]] < nums[i % n]) {
+          ret[stk[stk.length - 1]] = nums[i % n];
+          stk.pop();
+      }
+      stk.push(i % n);
+  }
+  return ret;
+};
+
+// 131. 分割回文串 晦朔 + 动态规划
+/**
+ * @param {string} s
+ * @return {string[][]}
+ */
+var partition = function(s) {
+  const dfs = (i) => {
+      if (i === n) {
+          ret.push(ans.slice());
+          return;
+      }
+      for (let j = i; j < n; ++j) {
+          if (f[i][j]) {
+              ans.push(s.slice(i, j + 1));
+              dfs(j + 1);
+              ans.pop();
+          }
+      }
+  }
+  
+  const n = s.length;
+  const f = new Array(n).fill(0).map(() => new Array(n).fill(true));
+  let ret = [], ans = [];
+  
+  for (let i = n - 1; i >= 0; --i) {
+      for (let j = i + 1; j < n; ++j) {
+          f[i][j] = (s[i] === s[j]) && f[i + 1][j - 1];
+      }
+  }
+  dfs(0);
+  return ret;
+};
+
+// 132. 分割回文串 II 动态规划  关键是预处理
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minCut = function(s) {
+  const n = s.length;
+  const g = new Array(n).fill(0).map(() => new Array(n).fill(true));
+
+  for (let i = n - 1; i >= 0; --i) {
+      for (let j = i + 1; j < n; ++j) {
+          g[i][j] = s[i] == s[j] && g[i + 1][j - 1];
+      }
+  }
+
+  const f = new Array(n).fill(Number.MAX_SAFE_INTEGER);
+  for (let i = 0; i < n; ++i) {
+      if (g[0][i]) {
+          f[i] = 0;
+      } else {
+          for (let j = 0; j < i; ++j) {
+              if (g[j + 1][i]) {
+                  f[i] = Math.min(f[i], f[j] + 1);
+              }
+          }
+      }
+  }
+
+  return f[n - 1];
+};
