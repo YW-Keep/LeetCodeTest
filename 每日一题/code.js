41075bbf83cf034aedbdbdaca20c2393a28b224e
tang <tangyuwei@myhexin.com>
2021-03-22 14:55:36 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -5471,3 +5471,71 @@ ParkingSystem.prototype.addCar = function(carType) {
   }
   return false;
 };
+
+// 191. 位1的个数 基础题
+var hammingWeight = function(n) {
+  let ret = 0;
+  while (n) {
+      n &= n - 1;
+      ret++;
+  }
+  return ret;
+};
+
+// 73. 矩阵置零 基础题
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+ var setZeroes = function(matrix) {
+  const m = matrix.length, n = matrix[0].length;
+  const row = new Array(m).fill(false);
+  const col = new Array(n).fill(false);
+  for (let i = 0; i < m; i++) {
+      for (let j = 0; j < n; j++) {
+          if (matrix[i][j] === 0) {
+              row[i] = col[j] = true;
+          }
+      }
+  }
+  for (let i = 0; i < m; i++) {
+      for (let j = 0; j < n; j++) {
+          if (row[i] || col[j]) {
+              matrix[i][j] = 0;
+          }
+      }
+  }
+};
+
+// 150. 逆波兰表达式求值  堆栈
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+ var evalRPN = function(tokens) {
+  const stack = [];
+  const n = tokens.length;
+  for (let i = 0; i < n; i++) {
+      const token = tokens[i];
+      if (isNumber(token)) {
+          stack.push(parseInt(token));
+      } else {
+          const num2 = stack.pop();
+          const num1 = stack.pop();
+          if (token === '+') {
+              stack.push(num1 + num2);
+          } else if (token === '-') {
+              stack.push(num1 - num2);
+          } else if (token === '*') {
+              stack.push(num1 * num2);
+          } else if (token === '/') {
+              stack.push(num1 / num2 > 0 ? Math.floor(num1 / num2) : Math.ceil(num1 / num2));
+          }
+      }
+  }
+  return stack.pop();
+
+  function isNumber(token) {
+    return !('+' === token || '-' === token || '*' === token || '/' === token );
+  }
+};
