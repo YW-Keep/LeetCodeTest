b9c8894c3d61a2c4063e9154ede6f13fe2b78001
tang <tangyuwei@myhexin.com>
2021-03-23 09:20:51 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -5539,3 +5539,24 @@ var hammingWeight = function(n) {
     return !('+' === token || '-' === token || '*' === token || '/' === token );
   }
 };
+
+//  341. 扁平化嵌套列表迭代器 基础逻辑
+var NestedIterator = function(nestedList) {
+    this.stack = nestedList;
+};
+
+NestedIterator.prototype.hasNext = function() {
+    while (this.stack.length !== 0) {
+        if (this.stack[0].isInteger()) {
+            return true;
+        } else {
+            let cur = this.stack[0].getList();
+            this.stack.shift();
+            this.stack.unshift(...cur);
+        }
+    }
+};
+
+NestedIterator.prototype.next = function() {
+    return this.stack.shift().getInteger();
+};
