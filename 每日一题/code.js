2af3a86e0acbc2ca7c1914c29793751fba085ffd
tang <tangyuwei@myhexin.com>
2021-04-08 08:56:51 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -5985,3 +5985,21 @@ var search = function(nums, target) {
   }
   return false;
 };
+
+// 153. 寻找旋转排序数组中的最小值 二分查找
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var findMin = function(nums) {
+   let i = 0, j = nums.length -1
+   while(i < j) {
+     let mid  = Math.floor((i +j)/2)
+     if(nums[mid] <nums[j]) {
+       j = mid
+     } else {
+       i = mid +1;
+     }
+   }
+   return nums[i]
+};
