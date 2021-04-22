e7fd135a72732dde0e3d5fd1bd5623700eaa39fb
tang <tangyuwei@myhexin.com>
2021-04-22 09:48:17 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -6346,3 +6346,36 @@ const getID = (x, w) => {
   }
   return f[n];
 };
+
+// 363. 矩形区域不超过 K 的最大数值和  遍历
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+ var maxSumSubmatrix = function(matrix, max) {
+  let row = matrix.length // 行
+  let column = matrix[0].length // 列
+  let b = Array.from({length: column}, () => 0) // 存储每列之和
+  let res = -Number.MAX_VALUE
+  for(let i=0;i<row;i++) { // 遍历开始行
+      for(let t=0;t<b.length;t++) b[t] = 0 // 开始行改变之后需要把每列之和置零
+      for(let j=i;j<row;j++) {
+          for(let k=0;k<column;k++) b[k] += matrix[j][k]
+          // 把所有可能遍历出来
+          for(let m=0;m<b.length;m++) {
+              let sum = 0
+              for(let n=m;n<b.length;n++){
+                  sum += b[n]
+                  if(sum <= max && sum > res) { // 只有小于max，且大于之前的值
+                      res = sum
+                      if(res == max) {
+                        return max
+                      }
+                  }
+              }
+          }
+      }
+  }
+  return res
+};
