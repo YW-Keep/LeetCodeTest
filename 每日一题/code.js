8efd7444f54c449d6e24b0ce6202fac908a078bd
tang <tangyuwei@myhexin.com>
2021-04-26 10:11:10 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -6481,3 +6481,31 @@ var combinationSum4 = function(nums, target) {
     }
     return backup[target];
 };
+
+// 1011. 在 D 天内送达包裹的能力 二分查找，确定左右边界
+/**
+ * @param {number[]} weights
+ * @param {number} D
+ * @return {number}
+ */
+ var shipWithinDays = function(weights, D) {
+  let left = Math.max(...weights), right = weights.reduce((a, b) => a + b);
+  while (left < right) {
+      const mid = Math.floor((left + right) / 2);
+      let need = 1, cur = 0;
+      for (const weight of weights) {
+          if (cur + weight > mid) {
+              need++;
+              cur = 0;
+          } 
+          cur += weight;
+      }
+
+      if (need <= D) {
+          right = mid;
+      } else {
+          left = mid + 1;
+      }
+  }
+  return left;
+};
