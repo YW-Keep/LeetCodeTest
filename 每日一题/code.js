3ad4559532195928701177e39002afb8948249dd
tang <tangyuwei@myhexin.com>
2021-04-27 09:29:01 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -6509,3 +6509,39 @@ var combinationSum4 = function(nums, target) {
   }
   return left;
 };
+
+// 938. 二叉搜索树的范围和 中序遍历
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+ var rangeSumBST = function(root, low, high) {
+   let sum = 0
+   dfs(root)
+   return sum
+
+   function dfs (node) {
+     if(!node) {
+       return
+     }
+     if(node.left && node.val >= low) {
+       dfs(node.left)
+     }
+     if(node.val >= low && node.val <= high) {
+       sum += node.val
+     }
+     if(node.val <= high && node.right) {
+       dfs(node.right)
+     }
+   }
+};
