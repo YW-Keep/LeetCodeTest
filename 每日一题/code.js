25dd928aa34037017a6ef718393edf4f8193b800
tangyuwei <tangyuwei@myhexin.com>
2021-04-29 09:12:12 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -6567,3 +6567,35 @@ var combinationSum4 = function(nums, target) {
   }
   return false;
 };
+
+// 403. 青蛙过河  动态规划
+
+/**
+ * @param {number[]} stones
+ * @return {boolean}
+ */
+ var canCross = function(stones) {
+  const n = stones.length;
+  const dp = new Array(n).fill(0).map(() => new Array(n).fill(0));
+  dp[0][0] = true;
+  for (let i = 1; i < n; ++i) {
+      if (stones[i] - stones[i - 1] > i) {
+          return false;
+      }
+  }
+  
+  for (let i = 1; i < n; ++i) {
+      for (let j = i - 1; j >= 0; --j) {
+          const k = stones[i] - stones[j];
+          if (k > j + 1) {
+              break;
+          }
+          dp[i][k] = dp[j][k - 1] || dp[j][k] || dp[j][k + 1];
+          if (i === n - 1 && dp[i][k]) {
+              return true;
+          }
+      }
+  }
+  return false;
+};
+
