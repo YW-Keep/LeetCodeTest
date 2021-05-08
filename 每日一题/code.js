e83c959d7ff3bf549b108ae08a5b7aa9036effeb
tangyuwei <tangyuwei@myhexin.com>
2021-05-08 10:36:11 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -6704,3 +6704,64 @@ var leastBricks = function(wall) {
 　}); 
   return num - max;
 };
+
+// 7. 整数反转 基础数学题
+/**
+ * @param {number} x
+ * @return {number}
+ */
+ var reverse = function(x) {
+  let rev = 0;
+  while (x !== 0) {
+      const digit = x % 10;
+      x = ~~(x / 10);
+      rev = rev * 10 + digit;
+      if (rev < Math.pow(-2, 31) || rev > Math.pow(2, 31) - 1) {
+          return 0;
+      }
+  }
+  return rev;
+};
+
+// 1723. 完成所有工作的最短时间 二分查找
+var minimumTimeRequired = function(jobs, k) {
+    jobs.sort((a, b) => b - a);
+    let l = jobs[0], r = jobs.reduce(function(prev, curr, idx, jobs){ return prev + curr });
+    while (l < r) {
+        const mid = Math.floor((l + r) >> 1);
+        if (check(jobs, k, mid)) {
+            r = mid;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return l;
+};
+
+const check = (jobs, k, limit) => {
+    const workloads = new Array(k).fill(0);
+    return backtrack(jobs, workloads, 0, limit);
+}
+
+const backtrack = (jobs, workloads, i, limit) => {
+    if (i >= jobs.length) {
+        return true;
+    }
+    let cur = jobs[i];
+    for (let j = 0; j < workloads.length; ++j) {
+        if (workloads[j] + cur <= limit) {
+            workloads[j] += cur;
+            if (backtrack(jobs, workloads, i + 1, limit)) {
+                return true;
+            }
+            workloads[j] -= cur;
+        }
+        // 如果当前工人未被分配工作，那么下一个工人也必然未被分配工作
+        // 或者当前工作恰能使该工人的工作量达到了上限
+        // 这两种情况下我们无需尝试继续分配工作
+        if (workloads[j] === 0 || workloads[j] + cur === limit) {
+            break;
+        }
+    }
+    return false;
+}
