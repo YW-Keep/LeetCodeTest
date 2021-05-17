ca1842416466ac401204a49b7863fb2c03cc18c4
tangyuwei <tangyuwei@myhexin.com>
2021-05-17 09:50:59 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -6992,3 +6992,79 @@ var decode = function(encoded) {
   }
   return roman.join('');
 };
+
+// 993. 二叉树的堂兄弟节点
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean}
+ */
+ var isCousins = function(root, x, y) {
+  // x 的信息
+  let x_parent = null, x_depth = null, x_found = false;
+  // y 的信息
+  let y_parent = null, y_depth = null, y_found = false;
+  
+  const dfs = (node, depth, parent) => {
+      if (!node) {
+          return;
+      }
+      if (node.val === x) {
+          [x_parent, x_depth, x_found] = [parent, depth, true];
+      } else if (node.val === y) {
+          [y_parent, y_depth, y_found] = [parent, depth, true];
+      }
+
+      // 如果两个节点都找到了，就可以提前退出遍历
+      // 即使不提前退出，对最坏情况下的时间复杂度也不会有影响
+      if (x_found && y_found) {
+          return;
+      }
+
+      dfs(node.left, depth + 1, node);
+
+      if (x_found && y_found) {
+          return;
+      }
+
+      dfs(node.right, depth + 1, node);
+  }
+  dfs(root, 0, null);
+  return x_depth === y_depth && x_parent !== y_parent;
+};
+
+// 13. 罗马数字转整数 基础题
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+  const symbolValues = new Map();
+  symbolValues.set('I', 1);
+  symbolValues.set('V', 5);
+  symbolValues.set('X', 10);
+  symbolValues.set('L', 50);
+  symbolValues.set('C', 100);
+  symbolValues.set('D', 500);
+  symbolValues.set('M', 1000);  
+  let ans = 0;
+  const n = s.length;
+  for (let i = 0; i < n; ++i) {
+      const value = symbolValues.get(s[i]);
+      if (i < n - 1 && value < symbolValues.get(s[i + 1])) {
+          ans -= value;
+      } else {
+          ans += value;
+      }
+  }
+  return ans;
+};
