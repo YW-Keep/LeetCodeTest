517e30558d52bb5cd1af8173bfabcfc18f265510
tangyuwei <tangyuwei@myhexin.com>
2021-05-19 08:47:25 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -7105,3 +7105,24 @@ var countTriplets = function(arr) {
     }
     return ans;
 };
+
+// 1738. 找出第 K 大的异或坐标值  先计算。再排序取值
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+
+ var kthLargestValue = function(matrix, k) {
+  const m = matrix.length, n = matrix[0].length;
+  const pre = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+  const results = [];
+  for (let i = 1; i < m + 1; i++) {
+      for (let j = 1; j < n + 1; j++) {
+          pre[i][j] = pre[i - 1][j] ^ pre[i][j - 1] ^ pre[i - 1][j - 1] ^ matrix[i - 1][j - 1];
+          results.push(pre[i][j]);
+      }
+  }
+  results.sort((a, b) => b - a);
+  return results[k - 1];
+}
