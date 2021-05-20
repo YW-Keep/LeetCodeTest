46828f7c51e756832868786eaf052c03ad797dee
tangyuwei <tangyuwei@myhexin.com>
2021-05-20 09:36:55 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -7126,3 +7126,24 @@ var countTriplets = function(arr) {
   results.sort((a, b) => b - a);
   return results[k - 1];
 }
+
+// 692. 前K个高频单词  哈希表 排序
+/**
+ * @param {string[]} words
+ * @param {number} k
+ * @return {string[]}
+ */
+var topKFrequent = function(words, k) {
+  const cnt = new Map();
+  for (const word of words) {
+      cnt.set(word, (cnt.get(word) || 0) + 1);
+  }
+  const rec = [];
+  for (const entry of cnt.keys()) {
+      rec.push(entry);
+  }
+  rec.sort((word1, word2) => {
+      return cnt.get(word1) === cnt.get(word2) ? word1.localeCompare(word2) : cnt.get(word2) - cnt.get(word1);
+  })
+  return rec.slice(0, k);
+};
