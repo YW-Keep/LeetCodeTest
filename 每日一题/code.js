c1adf8f68ac250b7958104396dcec74b428f9377
tangyuwei <tangyuwei@myhexin.com>
2021-05-24 10:24:43 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -7170,3 +7170,45 @@ var topKFrequent = function(words, k) {
   }
   return dp[m][n];
 };
+
+// 664. 奇怪的打印机 动态规划
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var strangePrinter = function(s) {
+  const n = s.length;
+  const f = new Array(n).fill(0).map(() => new Array(n).fill(0));
+  for (let i = n - 1; i >= 0; i--) {
+      f[i][i] = 1;
+      for (let j = i + 1; j < n; j++) {
+          if (s[i] == s[j]) {
+              f[i][j] = f[i][j - 1];
+          } else {
+              let minn = Number.MAX_SAFE_INTEGER;
+              for (let k = i; k < j; k++) {
+                  minn = Math.min(minn, f[i][k] + f[k + 1][j]);
+              }
+              f[i][j] = minn;
+          }
+      }
+  }
+  return f[0][n - 1];
+};
+
+// 810. 黑板异或游戏 数学题
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+ var xorGame = function(nums) {
+  if (nums.length % 2 == 0) {
+      return true;
+  }
+  let xor = 0;
+  for (const num of nums) {
+      xor ^= num;
+  }
+  return xor == 0;
+};
