e19b39c15f7931d833cc51bbdc9ca4b4fbfd9352
tangyuwei <tangyuwei@myhexin.com>
2021-05-26 09:49:13 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -7285,3 +7285,36 @@ var topKFrequent = function(words, k) {
   dfs(root, p, q);
   return ans;
 };
+
+// 1190. 反转每对括号间的子串 预处理
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseParentheses = function(s) {
+    const n = s.length;
+    const pair = new Array(n).fill(0);
+    const stack = [];
+    for (let i = 0; i < n; i++) {
+        if (s[i] === '(') {
+            stack.push(i);
+        } else if (s[i] == ')') {
+            const j = stack.pop();
+            pair[i] = j;
+            pair[j] = i;
+        }
+    }
+
+    const sb = [];
+    let index = 0, step = 1;
+    while (index < n) {
+        if (s[index] === '(' || s[index] === ')') {
+            index = pair[index];
+            step = -step;
+        } else {
+            sb.push(s[index]);
+        }
+        index += step;
+    }
+    return sb.join('');
+};
