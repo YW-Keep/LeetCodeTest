2ce6b883e9d2a6bc27bba3a4784af91bfdbce690
tangyuwei <tangyuwei@myhexin.com>
2021-06-01 09:55:52 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -7351,3 +7351,30 @@ var reverseParentheses = function(s) {
 var isPowerOfTwo = function(n) {
     return n > 0 && (n & (n - 1)) === 0;
 };
+
+// 1744. 你能在你最喜欢的那天吃到你最喜欢的糖果吗？ 前缀和 基础题
+var canEat = function(candiesCount, queries) {
+  const n = candiesCount.length;
+  
+  // 前缀和
+  const sum = new Array(n).fill(0);;
+  sum[0] = candiesCount[0];
+  for (let i = 1; i < n; ++i) {
+      sum[i] = sum[i - 1] + candiesCount[i];
+  }
+  
+  const q = queries.length;
+  const ans = new Array(q).fill(0);
+  for (let i = 0; i < q; ++i) {
+      const query = queries[i];
+      const favoriteType = query[0], favoriteDay = query[1], dailyCap = query[2];
+      
+      const x1 = favoriteDay + 1;
+      const y1 = (favoriteDay + 1) * dailyCap;
+      const x2 = favoriteType == 0 ? 1 : sum[favoriteType - 1] + 1;
+      const y2 = sum[favoriteType];
+      
+      ans[i] = !(x1 > y2 || y1 < x2);
+  }
+  return ans;
+};
