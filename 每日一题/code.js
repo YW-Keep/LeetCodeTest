84ba2d59db55ab3bf8e4eb70e5f41b53beede343
tangyuwei <tangyuwei@myhexin.com>
2021-06-08 09:21:21 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -7558,4 +7558,28 @@ var canEat = function(candiesCount, queries) {
 
 };
 
+//  1049. 最后一块石头的重量 II 动态规划
 
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+ var lastStoneWeightII = function(stones) {
+  let sum = 0;
+  for (const weight of stones) {
+      sum += weight;
+  }
+  const m = Math.floor(sum / 2);
+  const dp = new Array(m + 1).fill(false);
+  dp[0] = true;
+  for (const weight of stones) {
+      for (let j = m; j >= weight; --j) {
+          dp[j] = dp[j] || dp[j - weight];
+      }
+  }
+  for (let j = m;; --j) {
+      if (dp[j]) {
+          return sum - 2 * j;
+      }
+  }
+};
