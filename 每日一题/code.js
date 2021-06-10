5f149f49fcf0b9bd62aa93b17b1e05aa0efdb294
tangyuwei <tangyuwei@myhexin.com>
2021-06-10 09:26:31 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -7609,3 +7609,21 @@ var canEat = function(candiesCount, queries) {
   }
   return dp[n][minProfit];
 };
+
+
+// 518. 零钱兑换 II  动态规划
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+ var change = function(amount, coins) {
+  const dp = new Array(amount + 1).fill(0);
+  dp[0] = 1;
+  for (const coin of coins) {
+      for (let i = coin; i <= amount; i++) {
+          dp[i] += dp[i - coin];
+      }
+  }
+  return dp[amount];
+};
