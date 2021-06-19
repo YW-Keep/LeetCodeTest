6d6cc4277f496c02f197f743ed4a45badd33cf6a
tangyuwei <tangyuwei@myhexin.com>
2021-06-19 10:52:05 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -7759,7 +7759,7 @@ var isNumber = function(s) {
   return true;
 };
 
-// 483. 最小好进制
+// 483. 最小好进制  数学题
 /**
  * @param {string} n
  * @return {string}
@@ -7781,4 +7781,42 @@ var isNumber = function(s) {
       }
   }
   return (BigInt(n) - BigInt(1)) + '';
+};
+
+// 1239. 串联字符串的最大长度 位运算+回溯
+/**
+ * @param {string[]} arr
+ * @return {number}
+ */
+var maxLength = function(arr) {
+    let ans = 0;
+    const masks = [];
+    for (const s of arr) {
+        let mask = 0;
+        for (let i = 0; i < s.length; ++i) {
+            const ch = s[i].charCodeAt() - 'a'.charCodeAt();
+            if (((mask >> ch) & 1) !== 0) { // 若 mask 已有 ch，则说明 s 含有重复字母，无法构成可行解
+                mask = 0;
+                break;
+            }
+            mask |= 1 << ch; // 将 ch 加入 mask 中
+        }
+        if (mask > 0) {
+            masks.push(mask);
+        }
+    }
+
+    const backtrack = (masks, pos, mask) => {
+        if (pos === masks.length) {
+            ans = Math.max(ans, mask.toString(2).split('0').join('').length);
+            return;
+        }
+        if ((mask & masks[pos]) === 0) { // mask 和 masks[pos] 无公共元素
+            backtrack(masks, pos + 1, mask | masks[pos]);
+        }
+        backtrack(masks, pos + 1, mask);
+    }
+
+    backtrack(masks, 0, 0);
+    return ans;
 };
