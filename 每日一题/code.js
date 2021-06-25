0ada77360faea9f3af5019f18b93708ec857804d
tangyuwei <tangyuwei@myhexin.com>
2021-06-25 09:29:28 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -7937,3 +7937,70 @@ var maxLength = function(arr) {
   }
 
 };
+
+// 752. 打开转盘锁 广度优先搜索 
+/**
+ * @param {string[]} deadends
+ * @param {string} target
+ * @return {number}
+ */
+ var openLock = function(deadends, target) {
+  if (target === '0000') {
+      return 0;
+  }
+
+  const dead = new Set(deadends);
+  if (dead.has("0000")) {
+      return -1;
+  }
+
+  let step = 0;
+  const queue = [];
+  queue.push("0000");
+  const seen = new Set();
+  seen.add("0000");
+
+  while (queue.length) {
+      ++step;
+      const size = queue.length;
+      for (let i = 0; i < size; ++i) {
+          const status = queue.shift();
+          for (const nextStatus of get(status)) {
+              if (!seen.has(nextStatus) && !dead.has(nextStatus)) {
+                  if (nextStatus === target) {
+                      return step;
+                  }
+                  queue.push(nextStatus);
+                  seen.add(nextStatus);
+              }
+          }
+      }
+  }
+
+  return -1;
+
+  function numPrev(x) {
+    return x === '0' ? '9' : (parseInt(x) - 1) + '';
+  }
+  
+  function numSucc (x) {
+    return x === '9' ? '0' : (parseInt(x) + 1) + '';
+  }
+  
+  // 枚举 status 通过一次旋转得到的数字
+  function get(status) {
+    const ret = [];
+    const array = Array.from(status);
+    for (let i = 0; i < 4; ++i) {
+        const num = array[i];
+        array[i] = numPrev(num);
+        ret.push(array.join(''));
+        array[i] = numSucc(num);
+        ret.push(array.join(''));
+        array[i] = num;
+    }
+  
+    return ret;
+  }
+};
+
