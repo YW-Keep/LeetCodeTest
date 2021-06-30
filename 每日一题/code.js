033db1fae8d67aa38971931cdea999b970168c91
tangyuwei <tangyuwei@myhexin.com>
2021-06-30 11:05:51 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -8055,3 +8055,45 @@ var snakesAndLadders = function(board) {
   }
   return sb.reverse().join('');
 };
+
+// 剑指 Offer 37. 序列化二叉树  基础逻辑
+
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var serialize = function(root) {
+    return rserialize(root, '');
+};
+
+var deserialize = function(data) {
+    const dataArray = data.split(",");
+    return rdeserialize(dataArray);
+};
+
+const rserialize = (root, str) => {
+    if (root === null) {
+        str += "None,";
+    } else {
+        str += root.val + '' + ",";
+        str = rserialize(root.left, str);
+        str = rserialize(root.right, str);
+    }
+    return str;
+}
+
+const rdeserialize = (dataList) => {
+    if (dataList[0] === "None") {
+        dataList.shift();
+        return null;
+    }
+
+    const root = new TreeNode(parseInt(dataList[0]));
+    dataList.shift();
+    root.left = rdeserialize(dataList);
+    root.right = rdeserialize(dataList);
+
+    return root;
+}
