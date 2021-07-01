9c9f8b7de00b5b69f58882e35ee57b8ab13cd59d
tangyuwei <tangyuwei@myhexin.com>
2021-07-01 09:17:47 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -8097,3 +8097,24 @@ const rdeserialize = (dataList) => {
 
     return root;
 }
+ 
+// LCP 07. 传递信息 动态规划
+
+/**
+ * @param {number} n
+ * @param {number[][]} relation
+ * @param {number} k
+ * @return {number}
+ */
+ var numWays = function(n, relation, k) {
+  let dp = new Array(n).fill(0);
+  dp[0] = 1;
+  for (let i = 0; i < k; i++) {
+      const next = new Array(n).fill(0);
+      for (const [src, dst] of relation) {
+          next[dst] += dp[src];
+      }
+      dp = next;
+  }
+  return dp[n - 1];
+};
