5a9ed152d552fb4b86f5ca9946c055af241fc617
tangyuwei <tangyuwei@myhexin.com>
2021-07-07 10:48:44 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -8310,3 +8310,29 @@ var displayTable = function(orders) {
   }
   return table;
 };
+
+// 1711. 大餐计数 map
+/**
+ * @param {number[]} deliciousness
+ * @return {number}
+ */
+ var countPairs = function(deliciousness) {
+  const MOD = 1000000007;
+  let maxVal = 0;
+  for (const val of deliciousness) {
+      maxVal = Math.max(maxVal, val);
+  }
+  const maxSum = maxVal * 2;
+  let pairs = 0;
+  const map = new Map();
+  const n = deliciousness.length;
+  for (let i = 0; i < n; i++) {
+      const val = deliciousness[i];
+      for (let sum = 1; sum <= maxSum; sum <<= 1) {
+          const count = map.get(sum - val) || 0;
+          pairs = (pairs + count) % MOD;
+      }
+      map.set(val, (map.get(val) || 0) + 1);
+  }
+  return pairs;
+};
