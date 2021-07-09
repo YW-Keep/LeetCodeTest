d399cb1653603269f111f04920b7ad40e64556d9
tangyuwei <tangyuwei@myhexin.com>
2021-07-09 09:18:23 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -8354,3 +8354,31 @@ var displayTable = function(orders) {
    return ret;
 
 };
+
+// 面试题 17.10. 主要元素 Boyer-Moore投票算法
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var majorityElement = function(nums) {
+  let candidate = -1;
+  let count = 0;
+  for (const num of nums) {
+      if (count === 0) {
+          candidate = num;
+      }
+      if (num === candidate) {
+          count++;
+      } else {
+          count--;
+      }
+  }
+  count = 0;
+  const length = nums.length;
+  for (const num of nums) {
+      if (num === candidate) {
+          count++;
+      }
+  }
+  return count * 2 > length ? candidate : -1;
+};
