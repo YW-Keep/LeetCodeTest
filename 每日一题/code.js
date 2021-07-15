e00f28ace5477c62521689147612545777ec280c
tangyuwei <tangyuwei@myhexin.com>
2021-07-15 10:21:08 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -8492,3 +8492,17 @@ var hIndex = function(citations) {
 
 };
 
+// 1846. 减小和重新排列数组后的最大元素 排序
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maximumElementAfterDecrementingAndRearranging = function(arr) {
+    const n = arr.length;
+    arr.sort((a, b) => a - b);
+    arr[0] = 1;
+    for (let i = 1; i < n; ++i) {
+        arr[i] = Math.min(arr[i], arr[i - 1] + 1);
+    }
+    return arr[n - 1];
+};
