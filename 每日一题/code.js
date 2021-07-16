8d8bd01e051f9d93a4836e786bac735e35ac5103
tangyuwei <tangyuwei@myhexin.com>
2021-07-16 09:19:58 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -8506,3 +8506,35 @@ var maximumElementAfterDecrementingAndRearranging = function(arr) {
     }
     return arr[n - 1];
 };
+
+
+// 剑指 Offer 53 - I. 在排序数组中查找数字 I 二分查找
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+var search = function(nums, target) {
+  let ans = 0;
+  const leftIdx = binarySearch(nums, target, true);
+  const rightIdx = binarySearch(nums, target, false) - 1;
+  if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] === target && nums[rightIdx] === target) {
+      ans = rightIdx - leftIdx + 1;
+  } 
+  return ans;
+  function binarySearch(nums, target, lower) {
+    let left = 0, right = nums.length - 1, ans = nums.length;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] > target || (lower && nums[mid] >= target)) {
+            right = mid - 1;
+            ans = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return ans;
+  }
+
+};
