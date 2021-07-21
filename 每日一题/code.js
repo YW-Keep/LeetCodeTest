a015ecf4cce3bbc5aba7d7f975b03aa42a0de283
tangyuwei <tangyuwei@myhexin.com>
2021-07-21 10:52:13 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -8622,3 +8622,30 @@ var maxFrequency = function(nums, k) {
   }
   return res;
 };
+
+// 剑指 Offer 52. 两个链表的第一个公共节点 双指针
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+ var getIntersectionNode = function(headA, headB) {
+  if (headA === null || headB === null) {
+      return null;
+  }
+  let pA = headA, pB = headB;
+  while (pA !== pB) {
+      pA = pA === null ? headB : pA.next;
+      pB = pB === null ? headA : pB.next;
+  }
+  return pA;
+};
+
