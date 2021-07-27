83e8fe4a745f0fa9dc499938038023c27b27afbd
tangyuwei <tangyuwei@myhexin.com>
2021-07-27 09:07:04 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -8794,4 +8794,39 @@ var minOperations = function(target, arr) {
   }
 };
 
+// 671. 二叉树中第二小的节点  遍历
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var findSecondMinimumValue = function(root) {
+   let ans = -1;
+   const rootValue = root.val
+   dfs(root);
+   return ans;
+
+   function dfs(node) {
+     if(node == null) {
+       return null
+     }
+     if(ans !== -1 && node.val >= ans) {
+       return;
+     }
+     if(node.val > rootValue) {
+       ans = node.val
+     }
+     dfs(node.left)
+     dfs(node.right)
+   }
+
+};
+
 
