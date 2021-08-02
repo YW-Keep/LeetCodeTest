123c76bf3672a3ab2f2f44f2e5dae42860b1aaf1
tangyuwei <tangyuwei@myhexin.com>
2021-08-02 09:20:26 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -8972,4 +8972,51 @@ var titleToNumber = function(columnTitle) {
   }
 }
 
+// 1337. 矩阵中战斗力最弱的 K 行
+/**
+ * @param {number[][]} mat
+ * @param {number} k
+ * @return {number[]}
+ */
+var kWeakestRows = function (mat, k) {
+  const map = new Map();
+  mat.forEach((a, i) => map.set(a, i));
+  mat.sort();
+  return Array.from({ length: k }, (_, i) => map.get(mat[i]));
+};
+
+// 743. 网络延迟时间 Dijkstra 算法 没次取最近的点计算
+/**
+ * @param {number[][]} times
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+ var networkDelayTime = function(times, n, k) {
+  const INF = Number.MAX_SAFE_INTEGER;
+  const g = new Array(n).fill(INF).map(() => new Array(n).fill(INF));
+  for (const t of times) {
+      const x = t[0] - 1, y = t[1] - 1;
+      g[x][y] = t[2];
+  }
+
+  const dist = new Array(n).fill(INF);
+  dist[k - 1] = 0;
+  const used = new Array(n).fill(false);
+  for (let i = 0; i < n; ++i) {
+      let x = -1;
+      for (let y = 0; y < n; ++y) {
+          if (!used[y] && (x === -1 || dist[y] < dist[x])) {
+              x = y;
+          }
+      }
+      used[x] = true;
+      for (let y = 0; y < n; ++y) {
+          dist[y] = Math.min(dist[y], dist[x] + g[x][y]);
+      }
+  }
+
+  let ans = Math.max(...dist);
+  return ans === INF ? -1 : ans;
+};
 
