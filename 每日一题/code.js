feb1219de5b443b565164b005e4d42085a6fb452
tangyuwei <tangyuwei@myhexin.com>
2021-08-05 09:58:58 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -9065,3 +9065,36 @@ var kWeakestRows = function (mat, k) {
   }
   return ans;
 };
+
+//  802. 找到最终的安全状态 深度优先搜索 + 三色标记法
+/**
+ * @param {number[][]} graph
+ * @return {number[]}
+ */
+ var eventualSafeNodes = function(graph) {
+  const n = graph.length;
+  const color = new Array(n).fill(0);
+  const ans = [];
+  for (let i = 0; i < n; ++i) {
+      if (safe(graph, color, i)) {
+          ans.push(i);
+      }
+  }
+  return ans;
+
+
+  function safe(graph, color, x) {
+    if (color[x] > 0) {
+        return color[x] === 2;
+    }
+    color[x] = 1;
+    for (const y of graph[x]) {
+        if (!safe(graph, color, y)) {
+            return false;
+        }
+    }
+    color[x] = 2;
+    return true;
+  }
+};
+
