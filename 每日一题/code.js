c3055c4c564308f8169fbd8646d6b0141d6f230f
tangyuwei <tangyuwei@myhexin.com>
2021-08-12 09:47:13 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -9262,3 +9262,28 @@ var numberOfArithmeticSlices = function(nums) {
   return ans;
 };
 
+// 516. 最长回文子序列  动态规划
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var longestPalindromeSubseq = function(s) {
+  const n = s.length;
+  const dp = new Array(n).fill(0).map(() => new Array(n).fill(0));
+  for (let i = n - 1; i >= 0; i--) {
+      dp[i][i] = 1;
+      const c1 = s[i];
+      for (let j = i + 1; j < n; j++) {
+          const c2 = s[j];
+          if (c1 === c2) {
+              dp[i][j] = dp[i + 1][j - 1] + 2;
+          } else {
+              dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+          }
+      }
+  }
+  return dp[0][n - 1];
+};
+
+
+
