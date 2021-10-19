c5e5bf3ea1100c0d447201ab98ae8de971764606
tangyuwei <tangyuwei@myhexin.com>
2021-10-19 09:46:55 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -9360,3 +9360,64 @@ const N = parseInt(str, 2);
 return N ^ num;
 
 };
+
+// 211. 添加与搜索单词 - 数据结构设计 字典数
+var WordDictionary = function() {
+  this.trieRoot = new TrieNode();
+};
+
+WordDictionary.prototype.addWord = function(word) {
+  this.trieRoot.insert(word);
+};
+
+WordDictionary.prototype.search = function(word) {
+  const dfs = (index, node) => {
+      if (index === word.length) {
+          return node.isEnd;
+      }
+      const ch = word[index];
+      if (ch !== '.') {
+          const child = node.children[ch.charCodeAt() - 'a'.charCodeAt()]
+          if (child && dfs(index + 1, child)) {
+              return true;
+          }
+      } else {
+          for (const child of node.children) {
+              if (child && dfs(index + 1, child)) {
+                  return true;
+              }
+          }
+      }
+      return false;
+  }
+  
+  return dfs(0, this.trieRoot);
+};
+
+class TrieNode {
+  constructor() {
+      this.children = new Array(26).fill(0);
+      this.isEnd = false;
+  }
+
+  insert(word) {
+      let node = this;
+      for (let i = 0; i < word.length; i++) {
+          const ch = word[i];
+          const index = ch.charCodeAt() - 'a'.charCodeAt();
+          if (node.children[index] === 0) {
+              node.children[index] = new TrieNode();
+          }
+          node = node.children[index];
+      }
+      node.isEnd = true;
+  }
+
+  getChildren() {
+      return this.children;
+  }
+
+  isEnd() {
+      return this.isEnd;
+  }
+}
