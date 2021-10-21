a8a315968f65d134658793a9df59e572d404c5c8
tangyuwei <tangyuwei@myhexin.com>
2021-10-21 09:02:46 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -9435,3 +9435,26 @@ class TrieNode {
   }
   return res;
 };
+
+// 66. 加一 基础题
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+ var plusOne = function(digits) {
+  const n = digits.length;
+  for (let i = n - 1; i >= 0; --i) {
+      if (digits[i] !== 9) {
+          ++digits[i];
+          for (let j = i + 1; j < n; ++j) {
+              digits[j] = 0;
+          }
+          return digits;
+      }
+  }
+
+  // digits 中所有的元素均为 9
+  const ans = new Array(n + 1).fill(0);
+  ans[0] = 1;
+  return ans;
+};
