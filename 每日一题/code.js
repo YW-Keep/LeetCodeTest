5bfd90b96e934e97a84f30c73210b46981ef5ea7
tangyuwei <tangyuwei@myhexin.com>
2021-10-22 09:39:06 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -9458,3 +9458,54 @@ class TrieNode {
   ans[0] = 1;
   return ans;
 };
+
+
+// 229. 求众数 II 摩尔投票法
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var majorityElement = function(nums) {
+  let element1 = 0;
+  let element2 = 0;
+  let vote1 = 0;
+  let vote2 = 0;
+
+  for (const num of nums) {
+      if (vote1 > 0 && num === element1) { //如果该元素为第一个元素，则计数加1
+          vote1++;
+      } else if (vote2 > 0 && num === element2) { //如果该元素为第二个元素，则计数加1
+          vote2++;
+      } else if (vote1 === 0) { // 选择第一个元素
+          element1 = num;
+          vote1++;
+      } else if (vote2 === 0) { // 选择第二个元素
+          element2 = num;
+          vote2++;
+      } else { //如果三个元素均不相同，则相互抵消1次
+          vote1--;
+          vote2--;
+      }
+  }
+
+  let cnt1 = 0;
+  let cnt2 = 0;
+  for (const num of nums) {
+      if (vote1 > 0 && num === element1) {
+          cnt1++;
+      }
+      if (vote2 > 0 && num === element2) {
+          cnt2++;
+      }
+  }
+  // 检测元素出现的次数是否满足要求
+  const ans = [];
+  if (vote1 > 0 && cnt1 > Math.floor(nums.length / 3)) {
+      ans.push(element1);
+  }
+  if (vote2 > 0 && cnt2 > Math.floor(nums.length / 3)) {
+      ans.push(element2);
+  }
+
+  return ans;
+};
