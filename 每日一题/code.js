d2e5bdb9ff035b2504933444859b770e62383df0
tangyuwei <tangyuwei@myhexin.com>
2021-10-27 10:12:08 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -9545,3 +9545,76 @@ class TrieNode {
   return false;
 };
 
+//  496. 下一个更大元素 I 单调栈
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+ var nextGreaterElement = function(nums1, nums2) {
+  const map = new Map();
+  const stack = [];
+  for (let i = nums2.length - 1; i >= 0; --i) {
+      const num = nums2[i];
+      while (stack.length && num >= stack[stack.length - 1]) {
+          stack.pop();
+      }
+      map.set(num, stack.length ? stack[stack.length - 1] : -1);
+      stack.push(num);
+  }
+  const res = new Array(nums1.length).fill(0).map((_, i) => map.get(nums1[i]));
+  return res;
+};
+
+//  638. 大礼包 递归遍历
+/**
+ * @param {number[]} price
+ * @param {number[][]} special
+ * @param {number[]} needs
+ * @return {number}
+ */
+ var shoppingOffers = function(price, special, needs) {
+  const memo = new Map();
+
+  // 记忆化搜索计算满足购物清单所需花费的最低价格 
+  const dfs = (price, special, curNeeds, filterSpecial, n) => {
+      if (!memo.has(curNeeds)) {
+          let minPrice = 0;
+          for (let i = 0; i < n; ++i) {
+              minPrice += curNeeds[i] * price[i]; // 不购买任何大礼包，原价购买购物清单中的所有物品
+          }
+          for (const curSpecial of filterSpecial) {
+              const specialPrice = curSpecial[n];
+              const nxtNeeds = [];
+              for (let i = 0; i < n; ++i) {
+                  if (curSpecial[i] > curNeeds[i]) { // 不能购买超出购物清单指定数量的物品
+                      break;
+                  }
+                  nxtNeeds.push(curNeeds[i] - curSpecial[i]);
+              }
+              if (nxtNeeds.length === n) { // 大礼包可以购买
+                  minPrice = Math.min(minPrice, dfs(price, special, nxtNeeds, filterSpecial, n) + specialPrice);
+              }
+          }
+          memo.set(curNeeds, minPrice);
+      }
+      return memo.get(curNeeds);
+  }
+
+  const n = price.length;
+
+  // 过滤不需要计算的大礼包，只保留需要计算的大礼包
+  const filterSpecial = [];
+  for (const sp of special) {
+      let totalCount = 0, totalPrice = 0;
+      for (let i = 0; i < n; ++i) {
+          totalCount += sp[i];
+          totalPrice += sp[i] * price[i];
+      }
+      if (totalCount > 0 && totalPrice > sp[n]) {
+          filterSpecial.push(sp);
+      }
+  }
+
+  return dfs(price, special, needs, filterSpecial, n);
+};
