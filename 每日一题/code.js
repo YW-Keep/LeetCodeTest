3ca592a2258037eb6763220118cc55beb4ca243d
tangyuwei <tangyuwei@myhexin.com>
2021-10-28 10:17:33 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -9618,3 +9618,29 @@ class TrieNode {
 
   return dfs(price, special, needs, filterSpecial, n);
 };
+
+// 869. 重新排序得到 2 的幂 预处理所有可能的值再做同样处理匹配
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+
+ var reorderedPowerOf2 = function(n) {
+  const powerOf2Digits = new Set();
+
+  for (let n = 1; n <= 1e9; n <<= 1) {
+      powerOf2Digits.add(countDigits(n));
+  }
+
+  return powerOf2Digits.has(countDigits(n));
+
+  function countDigits (n)  {
+  const cnt = new Array(10).fill(0);
+  while (n) {
+      cnt[n % 10]++;
+      n = Math.floor(n / 10);
+  }
+  return cnt.join('');
+}
+};
