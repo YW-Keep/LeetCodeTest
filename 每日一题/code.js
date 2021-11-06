42225972b2308ad17e7696cd8117033f4cc98b98
tangyuwei <tangyuwei@myhexin.com>
2021-11-06 11:09:56 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -9731,3 +9731,17 @@ var trap = function(height) {
   return ans;
 };
 
+// 268. 丢失的数字
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var missingNumber = function(nums) {
+  const n = nums.length;
+  let total = Math.floor(n * (n + 1) / 2);
+  let arrSum = 0;
+  for (let i = 0; i < n; i++) {
+      arrSum += nums[i];
+  }
+  return total - arrSum;
+};
