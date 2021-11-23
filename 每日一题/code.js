09d8bc440d467c9300ddc65643645f4ecb62797c
tangyuwei <tangyuwei@myhexin.com>
2021-11-23 09:49:48 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -10115,3 +10115,40 @@ Solution.prototype.shuffle = function() {
   }
   return this.nums;
 };
+
+// 859. 亲密字符串  逻辑分类判断
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+ var buddyStrings = function(s, goal) {
+  if (s.length != goal.length) {
+      return false;
+  }
+  
+  if (s === goal) {
+      const count = new Array(26).fill(0);
+      for (let i = 0; i < s.length; i++) {
+          count[s[i].charCodeAt() - 'a'.charCodeAt()]++;
+          if (count[s[i].charCodeAt() - 'a'.charCodeAt()] > 1) {
+              return true;
+          }
+      }
+      return false;
+  } else {
+      let first = -1, second = -1;
+      for (let i = 0; i < s.length; i++) {
+          if (s[i] !== goal[i]) {
+              if (first === -1)
+                  first = i;
+              else if (second === -1)
+                  second = i;
+              else
+                  return false;
+          }
+      }
+
+      return (second !== -1 && s[first] === goal[second] && s[second] === goal[first]);
+  }
+};
