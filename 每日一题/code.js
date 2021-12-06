c325ac035b439f16ba0e1d6a0b93924450512626
tangyuwei <tangyuwei@myhexin.com>
2021-12-06 09:19:42 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -10390,3 +10390,47 @@ Solution.prototype.reset = function() {
   }
   return result;
 };
+
+// 383. 赎金信 基础题
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+ var canConstruct = function(ransomNote, magazine) {
+  if (ransomNote.length > magazine.length) {
+      return false;
+  }
+  const cnt = new Array(26).fill(0);
+  for (const c of magazine) {
+      cnt[c.charCodeAt() - 'a'.charCodeAt()]++;
+  }
+  for (const c of ransomNote) {
+      cnt[c.charCodeAt() - 'a'.charCodeAt()]--;
+      if(cnt[c.charCodeAt() - 'a'.charCodeAt()] < 0) {
+          return false;
+      }
+  }
+  return true;
+};
+
+// 1816. 截断句子 基础题
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+ var truncateSentence = function(s, k) {
+  const n = s.length;
+  let end = 0, count = 0;
+  for (let i = 1; i <= n; i++) {
+      if (i === n || s[i] === ' ') {
+          count++;
+          if (count === k) {
+              end = i;
+              break;
+          }
+      }
+  }
+  return s.slice(0, end);
+};
