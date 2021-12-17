85029af694fcf1af325e1a8fc613a172f37a3512
tangyuwei <tangyuwei@myhexin.com>
2021-12-17 09:09:35 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -10726,3 +10726,57 @@ TopVotedCandidate.prototype.q = function(t) {
   return ans;
 };
 
+
+// 1610. 可见点的最大数目  窗口
+
+/**
+ * @param {number[][]} points
+ * @param {number} angle
+ * @param {number[]} location
+ * @return {number}
+ */
+ var visiblePoints = function(points, angle, location) {
+  let sameCnt = 0;
+  const polarDegrees = [];
+  let locationX = location[0];
+  let locationY = location[1];
+  for (let i = 0; i < points.length; ++i) {
+      const x = points[i][0];
+      const y = points[i][1];
+      if (x === locationX && y === locationY) {
+          sameCnt++;
+          continue;
+      }
+      const degree = Math.atan2(y - locationY, x - locationX);
+      polarDegrees.push(degree);
+  }
+  polarDegrees.sort((a, b) => a - b);
+
+  const m = polarDegrees.length;
+  for (let i = 0; i < m; ++i) {
+      polarDegrees.push(polarDegrees[i] + 2 * Math.PI);
+  }
+
+  let maxCnt = 0;
+  let right = 0;
+  const toDegree = angle * Math.PI / 180;
+  for (let i = 0; i < m; ++i) {
+      const curr = polarDegrees[i] + toDegree;
+      while (right < polarDegrees.length && polarDegrees[right] <= curr) {
+          right++;
+      }
+      maxCnt = Math.max(maxCnt, right - i);
+  }
+  return maxCnt + sameCnt;
+};
+
+
+// 1518. 换酒问题  数学题
+/**
+ * @param {number} numBottles
+ * @param {number} numExchange
+ * @return {number}
+ */
+ var numWaterBottles = function(numBottles, numExchange) {
+  return numBottles + Math.floor((numBottles - 1) / (numExchange - 1))
+};
