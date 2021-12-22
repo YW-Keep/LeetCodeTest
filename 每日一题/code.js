0d9ee4579f4f1a75507967c2785407618025e351
tangyuwei <tangyuwei@myhexin.com>
2021-12-22 15:39:09 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -10850,3 +10850,40 @@ TopVotedCandidate.prototype.q = function(t) {
   }
   return ans;
 };
+
+// 1154. 一年中的第几天 基础逻辑题 
+/**
+ * @param {string} date
+ * @return {number}
+ */
+ var dayOfYear = function(date) {
+  const year = +date.slice(0, 4);
+  const month = +date.slice(5, 7);
+  const day = +date.slice(8);
+
+  const amount = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+      ++amount[1];
+  }
+
+  let ans = 0;
+  for (let i = 0; i < month - 1; ++i) {
+      ans += amount[i];
+  }
+  return ans + day;
+};
+
+// 686. 重复叠加字符串匹配 基础题
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+ var repeatedStringMatch = function(a, b) {
+  const l = Math.ceil(b.length/a.length)
+  if(a.repeat(l).includes(b))
+      return l
+  if(a.repeat(l+1).includes(b))
+      return l + 1
+  return -1
+};
