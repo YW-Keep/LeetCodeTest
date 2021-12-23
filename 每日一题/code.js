ccbe78bd66c2484fea5cc824aaa86b0ce1564d12
tangyuwei <tangyuwei@myhexin.com>
2021-12-23 10:25:12 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -10887,3 +10887,56 @@ TopVotedCandidate.prototype.q = function(t) {
       return l + 1
   return -1
 };
+
+// 1044. 最长重复子串 二分查找 + Rabin-Karp 字符串编码
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var longestDupSubstring = function(S) {
+  let R=26,MOD=1e9+7;
+  let lo=0,hi=S.length-1,res='';
+  while(lo<hi){
+    let len=Math.ceil((lo+hi)/2);
+    let sub=rabinKarp(S,len);
+    if(sub!==''){
+      lo=len;
+      res=sub;
+    }else{
+      hi=len-1;
+    }
+  }
+  return res;
+
+  function rabinKarp(str,len){
+    let RM=1
+    // 等价于RM=Math.pow(R,M-1) % MOD
+    // 由于JS精度问题拆解计算
+    for(let i=1;i<len;i++){
+      RM=(RM*R) % MOD;
+    }
+    let map=new Map();
+    let num=0;
+    // 计算前len个字符串的散列值
+    for(let i=0;i<len;i++){
+      let code=str.charCodeAt(i)-97;
+      num=(num*R + code) % MOD;
+    }
+    map.set(num,0);
+    // 后续计算散列值
+    for(let i=0;i<str.length-len;i++){
+      let preCode=str.charCodeAt(i)-97,
+          curCode=str.charCodeAt(i+len)-97;
+      num=(num+MOD-(preCode*RM % MOD)) % MOD;
+      num=(num*R + curCode) % MOD;
+      if(map.has(num)){
+        let sub=str.substring(i+1,i+1+len);
+        let preId=map.get(num),
+            preSub=str.substring(preId,preId+len);
+        if(sub===preSub)return sub;
+      }
+      map.set(num,i+1);
+    }
+    return '';
+  }
+};
