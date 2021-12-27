aefe0da45252e8d35e2eef3c7d1c8da83447dce1
tangyuwei <tangyuwei@myhexin.com>
2021-12-27 10:36:55 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -10982,3 +10982,87 @@ TopVotedCandidate.prototype.q = function(t) {
   }
   return eat;
 };
+
+// 825. 适龄的朋友 逻辑题
+/**
+ * @param {number[]} ages
+ * @return {number}
+ */
+ var numFriendRequests = function(ages) {
+  const n = ages.length;
+  ages.sort((a, b) => a - b);
+  let left = 0, right = 0, ans = 0;
+  for (const age of ages) {
+      if (age < 15) {
+          continue;
+      }
+      while (ages[left] <= 0.5 * age + 7) {
+          ++left;
+      }
+      while (right + 1 < n && ages[right + 1] <= age) {
+          ++right;
+      }
+      ans += right - left;
+  }
+  return ans;
+};
+
+// 1078. Bigram 分词 基础题
+/**
+ * @param {string} text
+ * @param {string} first
+ * @param {string} second
+ * @return {string[]}
+ */
+ var findOcurrences = function(text, first, second) {
+  const words = text.split(" ");
+  const list = [];
+  for (let i = 2; i < words.length; i++) {
+      if (words[i - 2] === first && words[i - 1] === second) {
+          list.push(words[i]);
+      }
+  }
+  return list;
+};
+
+// 1609. 奇偶树 遍历
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+ var isEvenOddTree = function(root) {
+  const queue = [];
+  queue.push(root);
+  let level = 0;
+  while (queue.length) {
+      const size = queue.length;
+      let prev = level % 2 == 0 ? -Number.MAX_VALUE : Number.MAX_VALUE;
+      for (let i = 0; i < size; i++) {
+          const node = queue.shift();
+          const value = node.val;
+          if (level % 2 === value % 2) {
+              return false;
+          }
+          if ((level % 2 === 0 && value <= prev) || (level % 2 === 1 && value >= prev)) {
+              return false;
+          }
+          prev = value;
+          if (node.left) {
+              queue.push(node.left);
+          }
+          if (node.right) {
+              queue.push(node.right);
+          }
+      }
+      level++;
+  }
+  return true;
+};
