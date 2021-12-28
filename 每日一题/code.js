6dc6309d141ebc1fd2bbf92fa65355f4642ae91b
tangyuwei <tangyuwei@myhexin.com>
2021-12-28 10:25:50 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -11066,3 +11066,33 @@ TopVotedCandidate.prototype.q = function(t) {
   }
   return true;
 };
+
+// 472. 连接词 集合+遍历
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+ var findAllConcatenatedWordsInADict = function (words) {
+  let set = new Set();
+  let res = [];
+  let getWord = function (word, start) {
+    for (let str = '', len = word.length - 1; start <= len; ++start) {
+      str += word[start]
+      if (set.has(str) && (start === len || getWord(word, start + 1))) {
+        return true;
+      }
+    }
+    return false;
+  }
+  words.sort((a, b) => a.length - b.length);
+  set.add(words[0]);
+  for (let i = 1, len = words.length; i < len; ++i) {
+    if (getWord(words[i], 0)) {
+      res.push(words[i])
+    } else {
+      set.add(words[i])
+    }
+  }
+  return res;
+};
+
