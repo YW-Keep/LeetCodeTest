340947ca26b5562bf12387151903d2f678cd6442
tangyuwei <tangyuwei@myhexin.com>
2022-01-05 10:42:43 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -11179,3 +11179,25 @@ TopVotedCandidate.prototype.q = function(t) {
   return ans;
 };
 
+// 1576. 替换所有的问号 基础题
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var modifyString = function(s) {
+  const n = s.length;
+  const arr = [...s];
+  for (let i = 0; i < n; ++i) {
+      if (arr[i] === '?') {
+          for (let j = 0; j < 3; ++j) {
+              if ((i > 0 && arr[i - 1] === String.fromCharCode('a'.charCodeAt() + j)) || (i < n - 1 && arr[i + 1] === String.fromCharCode('a'.charCodeAt() + j))) {
+                  continue;
+              }
+              arr[i] = String.fromCharCode('a'.charCodeAt() + j);
+              break;
+          }
+      }
+  }
+  return arr.join('');
+};
