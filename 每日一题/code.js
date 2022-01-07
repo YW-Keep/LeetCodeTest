5465acfc579c713624580a6c7e24b5134a9d01f6
tangyuwei <tangyuwei@myhexin.com>
2022-01-07 09:48:21 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -11223,3 +11223,22 @@ TopVotedCandidate.prototype.q = function(t) {
   
   return "/" + stack.join("/");
 };
+
+// 1614. 括号的最大嵌套深度 基础遍历
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var maxDepth = function(s) {
+  let ans = 0, size = 0;
+  for (let i = 0; i < s.length; ++i) {
+      const ch = s[i];
+      if (ch === '(') {
+          ++size;
+          ans = Math.max(ans, size);
+      } else if (ch === ')') {
+          --size;
+      }
+  }
+  return ans;
+};
