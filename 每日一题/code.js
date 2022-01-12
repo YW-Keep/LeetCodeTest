bc1ce8974de493650064e69644fa3d8c221db201
tangyuwei <tangyuwei@myhexin.com>
2022-01-12 11:37:32 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -11345,3 +11345,28 @@ const stringAdd = (s, firstStart, firstEnd, secondStart, secondEnd) => {
   }
   return ans;
 };
+
+
+// 334. 递增的三元子序列 贪心算法
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ var increasingTriplet = function(nums) {
+  const n = nums.length;
+  if (n < 3) {
+      return false;
+  }
+  let first = nums[0], second = Number.MAX_VALUE;
+  for (let i = 1; i < n; i++) {
+      const num = nums[i];
+      if (num > second) {
+          return true;
+      } else if (num > first) {
+          second = num;
+      } else {
+          first = num;
+      }
+  }
+  return false;
+};
