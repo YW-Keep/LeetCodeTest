223e6f9e8e7e25b4b4ae9ac8c50980186bdecd3b
tangyuwei <tangyuwei@myhexin.com>
2022-01-15 09:45:49 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -11456,3 +11456,24 @@ const stringAdd = (s, firstStart, firstEnd, secondStart, secondEnd) => {
   }
   return ans;
 }
+
+// 1716. 计算力扣银行的钱 等差数列求和
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var totalMoney = function(n) {
+  // 所有完整的周存的钱
+  const weekNumber = Math.floor(n / 7);
+  const firstWeekMoney = Math.floor((1 + 7) * 7 / 2);
+  const lastWeekMoney = firstWeekMoney + 7 * (weekNumber - 1);
+  const weekMoney = Math.floor((firstWeekMoney + lastWeekMoney) * weekNumber / 2);
+  // 剩下的不能构成一个完整的周的天数里存的钱
+  const dayNumber = n % 7;
+  const firstDayMoney = 1 + weekNumber;
+  const lastDayMoney = firstDayMoney + dayNumber - 1;
+  const dayMoney = Math.floor((firstDayMoney + lastDayMoney) * dayNumber / 2);
+  return weekMoney + dayMoney;
+};
+
+
