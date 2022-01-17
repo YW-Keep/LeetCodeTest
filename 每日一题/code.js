1b8ac403a444b27d628d186dfe3084c74fa8e555
tangyuwei <tangyuwei@myhexin.com>
2022-01-17 10:50:24 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -11477,3 +11477,57 @@ const stringAdd = (s, firstStart, firstEnd, secondStart, secondEnd) => {
 };
 
 
+// 1220. 统计元音字母序列的数目 动态规划
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var countVowelPermutation = function(n) {
+  const mod = 1000000007;
+  const dp = new Array(5).fill(0);
+  const ndp = new Array(5).fill(0);
+  for (let i = 0; i < 5; ++i) {
+      dp[i] = 1;
+  }
+  for (let i = 2; i <= n; ++i) {
+      /* a前面可以为e,u,i */
+      ndp[0] = (dp[1] + dp[2] + dp[4]) % mod;
+      /* e前面可以为a,i */
+      ndp[1] = (dp[0] + dp[2]) % mod;
+      /* i前面可以为e,o */
+      ndp[2] = (dp[1] + dp[3]) % mod;
+      /* o前面可以为i */
+      ndp[3] = dp[2];
+      /* u前面可以为i,o */
+      ndp[4] = (dp[2] + dp[3]) % mod;
+      dp.splice(0, 5, ...ndp);
+  }
+  let ans = 0;
+  for (let i = 0; i < 5; ++i) {
+      ans = (ans + dp[i]) % mod;
+  }
+  return ans;
+};
+
+// 382. 链表随机节点 转Array再取随机数
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ */
+ var Solution = function(head) {
+  this.list = [];
+  while (head != null) {
+      this.list.push(head.val);
+      head = head.next;
+  }
+};
+
+Solution.prototype.getRandom = function() {
+  return this.list[Math.floor(Math.random() * this.list.length)];
+};
