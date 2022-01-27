012ad55de03ebd6d0533223122e351f08413bab5
tangyuwei <tangyuwei@myhexin.com>
2022-01-27 10:10:13 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -11748,3 +11748,58 @@ DetectSquares.prototype.count = function(point) {
   }
   return res;
 };
+
+// 2047. 句子中的有效单词数 逻辑判断题
+/**
+ * @param {string} sentence
+ * @return {number}
+ */
+ var countValidWords = function(sentence) {
+  const n = sentence.length;
+  let l = 0, r = 0;
+  let ret = 0;
+  while (true) {
+      while (l < n && sentence[l] === ' ') {
+          l++;
+      }
+      if (l >= n) {
+          break;
+      }
+      r = l + 1;
+      while (r < n && sentence[r] != ' ') {
+          r++;
+      }
+      if (isValid(sentence.slice(l, r))) { // 判断根据空格分解出来的 token 是否有效
+          ret++;
+      }
+      l = r + 1;
+  }
+  return ret;
+
+  function isValid(word) {
+    const n = word.length;
+    let hasHyphens = false;
+    for (let i = 0; i < n; i++) {
+        if (word[i] >= '0' && word[i] <= '9') {
+            return false;
+        } else if (word[i] === '-') {
+            if (hasHyphens === true || i === 0 || i === n - 1 || !isLetter(word[i - 1]) || !isLetter(word[i + 1])) {
+                return false;
+            }
+            hasHyphens = true;
+        } else if (word[i] === '!' || word[i] === '.' || word[i] === ',') {
+            if (i !== n - 1) {
+                return false;
+            }
+        }
+    }
+    return true;
+
+    function isLetter(ch) {
+    if (ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z') {
+        return true;
+    }
+    return false;
+    }
+  }
+};
