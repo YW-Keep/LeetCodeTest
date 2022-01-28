bafc8134f74d39243cf26ce2a878c6cd82b4cc98
tangyuwei <tangyuwei@myhexin.com>
2022-01-28 10:38:18 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -11803,3 +11803,24 @@ DetectSquares.prototype.count = function(point) {
     }
   }
 };
+
+// 1996. 游戏中弱角色的数量 排序
+/**
+ * @param {number[][]} properties
+ * @return {number}
+ */
+ var numberOfWeakCharacters = function(properties) {
+  properties.sort((o1, o2) => {
+      return o1[0] === o2[0] ? (o1[1] - o2[1]) : (o2[0] - o1[0]);
+  });
+  let maxDef = 0;
+  let ans = 0;
+  for (const p of properties) {
+      if (p[1] < maxDef) {
+          ans++;
+      } else {
+          maxDef = p[1];
+      }
+  }
+  return ans;
+};
