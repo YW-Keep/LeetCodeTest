14565cfba182c5b7399dd688df959d947dce355d
tangyuwei <tangyuwei@myhexin.com>
2022-02-14 11:00:01 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -11965,3 +11965,21 @@ DetectSquares.prototype.count = function(point) {
 };
 
 
+// 540. 有序数组中的单一元素  二分查找
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var singleNonDuplicate = function(nums) {
+  let low = 0, high = nums.length - 1;
+  while (low < high) {
+      const mid = Math.floor((high - low) / 2) + low;
+      if (nums[mid] === nums[mid ^ 1]) {
+          low = mid + 1;
+      } else {
+          high = mid;
+      }
+  }
+  return nums[low];
+};
+
