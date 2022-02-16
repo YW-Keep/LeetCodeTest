13cb2c1a0ac5e2d57c398a351c0e252725fe4c0e
tangyuwei <tangyuwei@myhexin.com>
2022-02-16 10:15:05 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -12081,3 +12081,19 @@ var numEnclaves = function(grid) {
   }
   return ret;
 };
+
+
+// 面试题 17.16. 按摩师  动态规划 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var massage = function(nums) {
+  const len = nums.length;
+  if (!len) return 0;
+  const dp = [nums[0], Math.max(nums[0], nums[1])];
+  for (let i = 2; i < len; i++) {
+      dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+  }
+  return dp[len - 1];
+};
