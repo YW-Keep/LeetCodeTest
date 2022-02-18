52a50f78c0a0da7f7db2c1beb02a631f26c1a2f6
tangyuwei <tangyuwei@myhexin.com>
2022-02-18 09:41:52 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -12128,3 +12128,24 @@ var numEnclaves = function(grid) {
      }
      return dp[k][row][column];
  };
+
+
+// 1791. 找出星型图的中心节点  基础题
+/**
+ * @param {number[][]} edges
+ * @return {number}
+ */
+var findCenter = function(edges) {
+  const n = edges.length + 1;
+  const degrees = new Array(n + 1).fill(0);
+  for (const edge of edges) {
+      degrees[edge[0]]++;
+      degrees[edge[1]]++;
+  }
+  for (let i = 1; i <= n; i++) {
+      if (degrees[i] === n - 1) {
+          return i;
+      }
+  }
+  return 0;
+};
