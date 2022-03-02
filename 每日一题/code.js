c0c3972fd91a7174d6f9af3a9914c63b6f6308d9
tangyuwei <tangyuwei@myhexin.com>
2022-03-02 09:50:57 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -12376,3 +12376,34 @@ var complexNumberMultiply = function(num1, num2) {
   dfs(requests, 0);
   return ans;
 };
+
+// 6. Z 字形变换 数组记录 轮训添加
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+ var convert = function(s, numRows) {
+  const n = s.length, r = numRows;
+  if (r === 1 || r >= n) {
+      return s;
+  }
+  const mat = new Array(r).fill(0);
+  for (let i = 0; i < r; ++i) {
+      mat[i] = [];
+  }
+  for (let i = 0, x = 0, t = r * 2 - 2; i < n; ++i) {
+      mat[x].push(s[i]);
+      if (i % t < r - 1) {
+          ++x;
+      } else {
+          --x;
+      }
+  }
+  const ans = [];
+  for (const row of mat) {
+      ans.push(row.join(''));
+  }
+  return ans.join('');
+};
+
