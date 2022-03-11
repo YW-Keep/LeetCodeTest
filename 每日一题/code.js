5678b0890178688ac0480e9b465cabc5ebcbe3b0
tangyuwei <tangyuwei@myhexin.com>
2022-03-11 14:23:05 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -12595,3 +12595,48 @@ var complexNumberMultiply = function(num1, num2) {
    }
     
 };
+
+
+// 2049. 统计最高分的节点数目  深度遍历
+/**
+ * @param {number[]} parents
+ * @return {number}
+ */
+ var countHighestScoreNodes = function(parents) {
+  const n = parents.length;
+  const children = new Array(n).fill(0);
+  let maxScore = 0;
+  let cnt = 0;
+  for (let i = 0; i < n; i++) {
+      children[i] = [];
+  }
+  for (let i = 0; i < n; i++) {
+      const p = parents[i];
+      if (p !== -1) {
+          children[p].push(i);
+      }
+  }
+
+  const dfs = (node) => {
+      let score = 1;
+      let size = n - 1;
+      for (const c of children[node]) {
+          let t = dfs(c);
+          score *= t;
+          size -= t;
+      }
+      if (node !== 0) {
+          score *= size;
+      }
+      if (score === maxScore) {
+          cnt++;
+      } else if (score > maxScore) {
+          maxScore = score;
+          cnt = 1;
+      }
+      return n - size;
+  }
+
+  dfs(0);
+  return cnt;
+};
