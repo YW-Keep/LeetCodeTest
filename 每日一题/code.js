25a03d379516b27306464c07bef496507860d9f3
tangyuwei <tangyuwei@myhexin.com>
2022-03-23 09:16:44 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -12796,3 +12796,38 @@ Bank.prototype.withdraw = function(account, money) {
   }
   return maxIdleTime
 };
+
+
+// 440. 字典序的第K小数字 问题是给你一个数，找其中还有多少数字
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+ var findKthNumber = function(n, k) {
+  let curr = 1;
+  k--;
+  while (k > 0) {
+      const steps = getSteps(curr, n);
+      if (steps <= k) {
+          k -= steps;
+          curr++;
+      } else {
+          curr = curr * 10;
+          k--;
+      }
+  }
+  return curr;
+
+  function getSteps (curr, n) {
+      let steps = 0;
+      let first = curr;
+      let last = curr;
+      while (first <= n) {
+          steps += Math.min(last, n) - first + 1;
+          first = first * 10;
+          last = last * 10 + 9;
+      }
+      return steps;
+  };
+}
