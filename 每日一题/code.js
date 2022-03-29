3e39304692b70fd3341d4c8ecc06b22df1b50015
tangyuwei <tangyuwei@myhexin.com>
2022-03-29 09:33:55 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -12937,3 +12937,26 @@ Bank.prototype.withdraw = function(account, money) {
   }
   return missing;
 };
+
+// 2024. 考试的最大困扰度  窗口
+/**
+ * @param {string} answerKey
+ * @param {number} k
+ * @return {number}
+ */
+ var maxConsecutiveAnswers = function(answerKey, k) {
+  return Math.max(maxConsecutiveChar(answerKey, k, 'T'), maxConsecutiveChar(answerKey, k, 'F'));
+  function maxConsecutiveChar(answerKey, k, ch) {
+  const n = answerKey.length;
+  let ans = 0;
+  for (let left = 0, right = 0, sum = 0; right < n; right++) {
+      sum += answerKey.charAt(right) !== ch ? 1 : 0;
+      while (sum > k) {
+          sum -= answerKey[left++] !== ch ? 1 : 0;
+      }
+      ans = Math.max(ans, right - left + 1);
+  }
+  return ans;
+};
+}
+
