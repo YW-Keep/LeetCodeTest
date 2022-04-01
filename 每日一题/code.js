df0865cd86ace87ff96cd186da380468fa71f95f
tangyuwei <tangyuwei@myhexin.com>
2022-04-01 09:38:07 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -12987,3 +12987,32 @@ Bank.prototype.withdraw = function(account, money) {
       return true;
   };
 }
+
+// 954. 二倍数对数组 排序+map
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+ var canReorderDoubled = function(arr) {
+  const cnt = new Map();
+  for (const x of arr) {
+      cnt.set(x, (cnt.get(x) || 0) + 1);
+  }
+  if ((cnt.get(0) || 0) % 2 !== 0) {
+      return false;
+  }
+
+  const vals = [];
+  for (const x of cnt.keys()) {
+      vals.push(x);
+  }
+  vals.sort((a, b) => Math.abs(a) - Math.abs(b));
+
+  for (const x of vals) {
+      if ((cnt.get(2 * x) || 0) < cnt.get(x)) { // 无法找到足够的 2x 与 x 配对
+          return false;
+      }
+      cnt.set(2 * x, (cnt.get(2 * x) || 0) - cnt.get(x));
+  }
+  return true;
+};
