dd1730e4527d25167e1a8b696432a8ae2aa83775
tangyuwei <tangyuwei@myhexin.com>
2022-04-12 10:14:33 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -13389,3 +13389,19 @@ NumArray.prototype.prefixSum = function(index) {
   }
   return res;
 };
+
+//  806. 写字符串需要的行数 基础题
+var numberOfLines = function(widths, s) {
+  let lines = 1;
+  let width = 0;
+  for (let i = 0; i < s.length; i++) {
+      const need = widths[s[i].charCodeAt() - 'a'.charCodeAt()];
+      width += need;
+      if (width > 100) {
+          lines++;
+          width = need;
+      }
+  }
+  return [lines, width];
+};
+
