4d142393f5ed930559a41333daf7df92843d729f
tangyuwei <tangyuwei@myhexin.com>
2022-04-15 09:20:29 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -13453,3 +13453,45 @@ RandomizedSet.prototype.getRandom = function() {
   }
   return maxWealth;
 };
+
+// 385. 迷你语法分析器  基础遍历题
+/**
+ * @param {string} s
+ * @return {NestedInteger}
+ */
+ var deserialize = function(s) {
+  let index = 0;
+  const dfs = (s) => {
+      if (s[index] === '[') {
+          index++;
+          const ni = new NestedInteger();
+          while (s[index] !== ']') {
+              ni.add(dfs(s));
+              if (s[index] === ',') {
+                  index++;
+              }
+          }
+          index++;
+          return ni;
+      } else {
+          let negative = false;
+          if (s[index] === '-') {
+              negative = true;
+              index++;
+          }
+          let num = 0;
+          while (index < s.length && isDigit(s[index])) {
+              num = num * 10 + s[index].charCodeAt() - '0'.charCodeAt();
+              index++;
+          }
+          if (negative) {
+              num *= -1;
+          }
+          return new NestedInteger(num);
+      }
+  }
+  return dfs(s);
+  function isDigit(ch) {
+      return parseFloat(ch).toString() === "NaN" ? false : true;
+  }
+};
