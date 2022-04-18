be06da2c1ca7fa5bbdd191ee17d909dabf4271d0
tangyuwei <tangyuwei@myhexin.com>
2022-04-18 10:22:36 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -13517,3 +13517,27 @@ RandomizedSet.prototype.getRandom = function() {
   }
   return ret;
 };
+
+// 479. 最大回文数乘积 枚举
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var largestPalindrome = function(n) {
+  if (n === 1) {
+      return 9;
+  }
+  const upper = 10 ** n - 1;
+  for (let left = upper; left > upper / 10; left--) {
+      let right = String(left).split('').reverse().join('');
+      let p = BigInt(String(left) + right)    //得到回文数
+      let x = BigInt(upper);
+      while (x * x >= p) {
+          if (p % x === BigInt(0)) { // x 是 p 的因子
+              return p % BigInt(1337);
+          }
+          x--;
+      }
+  }
+};
+
