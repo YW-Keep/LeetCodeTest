312a1ebe78b9fbcb8579059f31d90ba8ee36d264
tangyuwei <tangyuwei@myhexin.com>
2022-04-19 10:00:50 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -13541,3 +13541,69 @@ RandomizedSet.prototype.getRandom = function() {
   }
 };
 
+
+// 819. 最常见的单词  集合映射
+/**
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ */
+ var mostCommonWord = function(paragraph, banned) {
+  const bannedSet = new Set();
+  for (const word of banned) {
+      bannedSet.add(word);
+  }
+  let maxFrequency = 0;
+  const frequencies = new Map();
+  let sb = '';
+  const length = paragraph.length;
+  for (let i = 0; i <= length; i++) {
+      if (i < length && isLetter(paragraph[i])) {
+          sb = sb + paragraph[i].toLowerCase();
+      } else if (sb.length > 0) {
+          if (!bannedSet.has(sb)) {
+              const frequency = (frequencies.get(sb) || 0) + 1;
+              frequencies.set(sb, frequency);
+              maxFrequency = Math.max(maxFrequency, frequency);
+          }
+          sb = '';
+      }
+  }
+  let mostCommon = "";
+  for (const [word, frequency] of frequencies.entries()) {
+      if (frequency === maxFrequency) {
+          mostCommon = word;
+          break;
+      }
+  }
+  return mostCommon;
+  function isLetter(ch) {
+      return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
+  }
+};
+
+// 821. 字符的最短距离 两次 遍历
+/**
+ * @param {string} s
+ * @param {character} c
+ * @return {number[]}
+ */
+ var shortestToChar = function(s, c) {
+  const n = s.length;
+  const ans = new Array(n).fill(0);
+
+  for (let i = 0, idx = -n; i < n; ++i) {
+      if (s[i] === c) {
+          idx = i;
+      }
+      ans[i] = i - idx;
+  }
+
+  for (let i = n - 1, idx = 2 * n; i >= 0; --i) {
+      if (s[i] == c) {
+          idx = i;
+      }
+      ans[i] = Math.min(ans[i], idx - i);
+  }
+  return ans;
+};
