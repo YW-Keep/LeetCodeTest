585b21e6d5717f4e6d96a3975b51b6fcf4263349
tangyuwei <tangyuwei@myhexin.com>
2022-04-22 09:33:43 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -13683,3 +13683,21 @@ RandomizedSet.prototype.getRandom = function() {
   }).join(" ")
 };
 
+
+//  396. 旋转函数 迭代
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var maxRotateFunction = function(nums) {
+  let f = 0, n = nums.length, numSum = _.sum(nums);
+  for (let i = 0; i < n; i++) {
+      f += i * nums[i];
+  }
+  let res = f;
+  for (let i = n - 1; i > 0; i--) {
+      f += numSum - n * nums[i];
+      res = Math.max(res, f);
+  }
+  return res;
+};
