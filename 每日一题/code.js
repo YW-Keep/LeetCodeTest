da58429f740abc90d6988fa85c9232617ee4888a
tangyuwei <tangyuwei@myhexin.com>
2022-04-28 13:49:22 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -13761,3 +13761,73 @@ Solution.prototype.pick = function(target) {
   const indices = this.pos.get(target);
   return indices[Math.floor(Math.random() * indices.length)];
 };
+
+// 417. 太平洋大西洋水流问题 深度遍历搜索
+/**
+ * @param {number[][]} heights
+ * @return {number[][]}
+ */
+ var pacificAtlantic = function(heights) {
+  let dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+  m = heights.length;
+  n = heights[0].length;
+  const pacific = new Array(m).fill(0).map(() => new Array(n).fill(0));
+  const atlantic = new Array(m).fill(0).map(() => new Array(n).fill(0));
+
+  const dfs = (row, col, ocean) => {
+      if (ocean[row][col]) {
+          return;
+      }
+      ocean[row][col] = true;
+      for (const dir of dirs) {
+          const newRow = row + dir[0], newCol = col + dir[1];
+          if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && heights[newRow][newCol] >= heights[row][col]) {
+              dfs(newRow, newCol, ocean);
+          }
+      }
+  };
+
+  for (let i = 0; i < m; i++) {
+      dfs(i, 0, pacific);
+  }
+  for (let j = 1; j < n; j++) {
+      dfs(0, j, pacific);
+  }
+  for (let i = 0; i < m; i++) {
+      dfs(i, n - 1, atlantic);
+  }
+  for (let j = 0; j < n - 1; j++) {
+      dfs(m - 1, j, atlantic);
+  }
+  const result = [];
+  for (let i = 0; i < m; i++) {
+      for (let j = 0; j < n; j++) {
+          if (pacific[i][j] && atlantic[i][j]) {
+              const cell = [];
+              cell.push(i);
+              cell.push(j);
+              result.push(cell);
+          }
+      }
+  }
+  return result;
+}
+
+// 905. 按奇偶排序数组 双指针
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var sortArrayByParity = function(nums) {
+  const n = nums.length;
+  const res = new Array(n).fill(0);
+  let left = 0, right = n - 1;
+  for (const num of nums) {
+      if (num % 2 === 0) {
+          res[left++] = num;
+      } else {
+          res[right--] = num;
+      }
+  }
+  return res;
+};
