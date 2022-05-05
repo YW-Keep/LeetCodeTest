03cf3cc19b07457e1613ad3d56d04cf3bc560ce1
tangyuwei <tangyuwei@myhexin.com>
2022-05-05 11:34:14 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -13831,3 +13831,23 @@ Solution.prototype.pick = function(target) {
   }
   return res;
 };
+
+// 713. 乘积小于 K 的子数组  滑动窗口
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+ var numSubarrayProductLessThanK = function(nums, k) {
+  let n = nums.length, ret = 0;
+  let prod = 1, i = 0;
+  for (let j = 0; j < n; j++) {
+      prod *= nums[j];
+      while (i <= j && prod >= k) {
+          prod /= nums[i];
+          i++;
+      }
+      ret += j - i + 1;
+  }
+  return ret;
+};
