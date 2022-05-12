75ef715c0d30e83bc5cab69137bb65c7bf22ad94
tangyuwei <tangyuwei@myhexin.com>
2022-05-12 11:27:40 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -13996,3 +13996,24 @@ var deserialize = function(data) {
 
   return construct(-Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, stack);
 };
+
+// 944. 删列造序 基础遍历
+/**
+ * @param {string[]} strs
+ * @return {number}
+ */
+ var minDeletionSize = function(strs) {
+  const row = strs.length;
+  const col = strs[0].length;
+  let ans = 0;
+  for (let j = 0; j < col; ++j) {
+      for (let i = 1; i < row; ++i) {
+          if (strs[i - 1][j] > strs[i][j]) {
+              ans++;
+              break;
+          }
+      }
+  }
+  return ans;
+};
+
