920db9f74dc3309841401d93b053f22063575fce
tangyuwei <tangyuwei@myhexin.com>
2022-05-13 09:54:32 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -14017,3 +14017,45 @@ var deserialize = function(data) {
   return ans;
 };
 
+// 面试题 01.05. 一次编辑 分类讨论
+/**
+ * @param {string} first
+ * @param {string} second
+ * @return {boolean}
+ */
+ var oneEditAway = function(first, second) {
+  const m = first.length, n = second.length;
+  if (n - m === 1) {
+      return oneInsert(first, second);
+  } else if (m - n === 1) {
+      return oneInsert(second, first);
+  } else if (m === n) {
+      let foundDifference = false;
+      for (let i = 0; i < m; i++) {
+          if (first[i] != second[i]) {
+              if (!foundDifference) {
+                  foundDifference = true;
+              } else {
+                  return false;
+              }
+          }
+      }
+      return true;
+  } else {
+      return false;
+  }
+  function oneInsert(shorter, longer) {
+      const length1 = shorter.length, length2 = longer.length;
+      let index1 = 0, index2 = 0;
+      while (index1 < length1 && index2 < length2) {
+          if (shorter[index1] == longer[index2]) {
+              index1++;
+          }
+          index2++;
+          if (index2 - index1 > 1) {
+              return false;
+          }
+      }
+      return true;
+  };
+}
