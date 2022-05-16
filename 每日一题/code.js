fc66ae1f7b303d4caed88a4ce7774eead27351d6
tangyuwei <tangyuwei@myhexin.com>
2022-05-16 09:57:38 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -14059,3 +14059,34 @@ var deserialize = function(data) {
       return true;
   };
 }
+
+//  面试题 04.06. 后继者 中序遍历
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @return {TreeNode}
+ */
+ var inorderSuccessor = function(root, p) {
+  const stack = [];
+  let prev = null, curr = root;
+  while (stack.length || curr) {
+      while (curr) {
+          stack.push(curr);
+          curr = curr.left;
+      }
+      curr = stack.pop();
+      if (prev === p) {
+          return curr;
+      }
+      prev = curr;
+      curr = curr.right;
+  }
+  return null;
+};
