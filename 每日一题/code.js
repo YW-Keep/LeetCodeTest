a68d10f6aca643016117b283b631d39142b46e64
tangyuwei <tangyuwei@myhexin.com>
2022-05-20 10:24:25 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -14148,3 +14148,50 @@ var deserialize = function(data) {
   return left;
 };
 
+
+// 462. 最少移动次数使数组元素相等 II 中位数均移动
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var minMoves2 = function(nums) {
+  nums.sort((a, b) => a - b);
+  let n = nums.length, ret = 0, x = nums[Math.floor(n / 2)];
+  for (let i = 0; i < n; i++) {
+      ret += Math.abs(nums[i] - x);
+  }
+  return ret;
+};
+
+// 436. 寻找右区间 二分查找
+/**
+ * @param {number[][]} intervals
+ * @return {number[]}
+ */
+ var findRightInterval = function(intervals) {
+  const n = intervals.length;
+  const startIntervals = new Array(n).fill(0).map(() => new Array(2).fill(0));
+  for (let i = 0; i < n; i++) {
+      startIntervals[i][0] = intervals[i][0];
+      startIntervals[i][1] = i;
+  }
+  startIntervals.sort((o1, o2) => o1[0] - o2[0]);
+
+  const ans = new Array(n).fill(0);
+  for (let i = 0; i < n; i++) {
+      let left = 0;
+      let right = n - 1;
+      let target = -1;
+      while (left <= right) {
+          const mid = Math.floor((left + right) / 2);
+          if (startIntervals[mid][0] >= intervals[i][1]) {
+              target = startIntervals[mid][1];
+              right = mid - 1;
+          } else {
+              left = mid + 1;
+          }
+      }
+      ans[i] = target;
+  }
+  return ans;
+};
