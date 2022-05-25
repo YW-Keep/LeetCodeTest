0adbe1af56b92123cf232a7c5dfa71bb7872dbc1
tangyuwei <tangyuwei@myhexin.com>
2022-05-25 09:57:28 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -14242,3 +14242,23 @@ var isUnivalTree = function(root) {
   }
   return true;
 };
+
+// 467. 环绕字符串中唯一的子字符串 动态规划
+/**
+ * @param {string} p
+ * @return {number}
+ */
+ var findSubstringInWraproundString = function(p) {
+  const dp = new Array(26).fill(0);
+  let k = 0;
+  for (let i = 0; i < p.length; ++i) {
+      if (i > 0 && (p[i].charCodeAt() - p[i - 1].charCodeAt() + 26) % 26 === 1) { // 字符之差为 1 或 -25
+          ++k;
+      } else {
+          k = 1;
+      }
+      dp[p[i].charCodeAt() - 'a'.charCodeAt()] = Math.max(dp[p[i].charCodeAt() - 'a'.charCodeAt()], k);
+  }
+  return _.sum(dp);
+};
+
