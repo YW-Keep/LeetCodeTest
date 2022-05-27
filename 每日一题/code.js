4b0cafd8d06e38df8b8d31e5374a2633c3f66182
tangyuwei <tangyuwei@myhexin.com>
2022-05-27 10:15:04 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -14286,3 +14286,28 @@ var isUnivalTree = function(root) {
   }
   return heights;
 };
+
+// 面试题 17.11. 单词距离 遍历记录
+/**
+ * @param {string[]} words
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+ var findClosest = function(words, word1, word2) {
+  const length = words.length;
+  let ans = length;
+  let index1 = -1, index2 = -1;
+  for (let i = 0; i < length; i++) {
+      const word = words[i];
+      if (word === word1) {
+          index1 = i;
+      } else if (word === word2) {
+          index2 = i;
+      }
+      if (index1 >= 0 && index2 >= 0) {
+          ans = Math.min(ans, Math.abs(index1 - index2));
+      }
+  }
+  return ans;
+};
