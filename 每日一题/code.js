aec3693b3d39d26b70b955a11c66c50e5802f36a
tangyuwei <tangyuwei@myhexin.com>
2022-06-02 10:09:07 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -14452,3 +14452,63 @@ var isUnivalTree = function(root) {
   }
   return dp[(1 << n) - 1] === 0;
 }
+
+// 450. 删除二叉搜索树中的节点 迭代
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+ var deleteNode = function(root, key) {
+  let cur = root, curParent = null;
+  while (cur && cur.val !== key) {
+      curParent = cur;
+      if (cur.val > key) {
+          cur = cur.left;
+      } else {
+          cur = cur.right;
+      }
+  }
+  if (!cur) {
+      return root;
+  }
+  if (!cur.left && !cur.right) {
+      cur = null;
+  } else if (!cur.right) {
+      cur = cur.left;
+  } else if (!cur.left) {
+      cur = cur.right;
+  } else {
+      let successor = cur.right, successorParent = cur;
+      while (successor.left) {
+          successorParent = successor;
+          successor = successor.left;
+      }
+      if (successorParent.val === cur.val) {
+          successorParent.right = successor.right;
+      } else {
+          successorParent.left = successor.right;
+      }
+      successor.right = cur.right;
+      successor.left = cur.left;
+      cur = successor;
+  }
+  if (!curParent) {
+      return cur;
+  } else {
+      if (curParent.left && curParent.left.val === key) {
+          curParent.left = cur;
+      } else {
+          curParent.right = cur;
+      }
+      return root;
+  }
+};
