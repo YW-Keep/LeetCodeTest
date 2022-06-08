6e901004a11e5c42b19b43721eaef05f86f911f3
tangyuwei <tangyuwei@myhexin.com>
2022-06-08 09:47:46 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -14563,3 +14563,14 @@ var isUnivalTree = function(root) {
   }
   return emailSet.size;
 };
+
+// 1037. 有效的回旋镖  判断不在一条直线上
+/**
+ * @param {number[][]} points
+ * @return {boolean}
+ */
+ var isBoomerang = function(points) {
+  const v1 = [points[1][0] - points[0][0], points[1][1] - points[0][1]];
+  const v2 = [points[2][0] - points[0][0], points[2][1] - points[0][1]];
+  return v1[0] * v2[1] - v1[1] * v2[0] != 0;
+};
