b7fe661b7fba153bf5aab4153f9d7d96b4ebc7c4
tangyuwei <tangyuwei@myhexin.com>
2022-06-15 10:10:37 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -14679,3 +14679,31 @@ var isUnivalTree = function(root) {
   }
   return res;
 };
+
+
+// 719. 找出第 K 小的数对距离 二分查找
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+ var smallestDistancePair = function(nums, k) {
+  nums.sort((a, b) => a - b);
+  let n = nums.length, left = 0, right = nums[n - 1] - nums[0];
+  while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+      let cnt = 0;
+      for (let i = 0, j = 0; j < n; j++) {
+          while (nums[j] - nums[i] > mid) {
+              i++;
+          }
+          cnt += j - i;
+      }
+      if (cnt >= k) {
+          right = mid - 1;
+      } else {
+          left = mid + 1;
+      }
+  }
+  return left;
+}
