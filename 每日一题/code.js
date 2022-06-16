0287764a5c4da2d579db06f7287de0716fff10e9
tangyuwei <tangyuwei@myhexin.com>
2022-06-16 09:47:22 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -14707,3 +14707,25 @@ var isUnivalTree = function(root) {
   }
   return left;
 }
+
+// 532. 数组中的 k-diff 数对  集合存储
+ 
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+ var findPairs = function(nums, k) {
+  const visited = new Set();
+  const res = new Set();
+  for (const num of nums) {
+      if (visited.has(num - k)) {
+          res.add(num - k);
+      }
+      if (visited.has(num + k)) {
+          res.add(num);
+      }
+      visited.add(num);
+  }
+  return res.size;
+};
