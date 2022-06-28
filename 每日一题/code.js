12f39925c7cbd32f70e5b5f3592296094fd60001
tangyuwei <tangyuwei@myhexin.com>
2022-06-28 09:46:13 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -14939,3 +14939,21 @@ var isUnivalTree = function(root) {
   }
   return parseInt(_.min(dp));
 };
+
+// 324. 摆动排序 II 排序
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+ var wiggleSort = function(nums) {
+  const arr = nums.slice();
+  arr.sort((a, b) => a - b);
+  const n = nums.length;
+  const x = Math.floor((n + 1) / 2);
+  for (let i = 0, j = x - 1, k = n - 1; i < n; i += 2, j--, k--) {
+      nums[i] = arr[j];
+      if (i + 1 < n) {
+          nums[i + 1] = arr[k];
+      }
+  }
+};
