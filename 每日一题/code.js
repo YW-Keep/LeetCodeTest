52a2ce45e5aaf37347500b6d89a86b50d7a2ddef
tangyuwei <tangyuwei@myhexin.com>
2022-06-29 11:21:14 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -14957,3 +14957,23 @@ var isUnivalTree = function(root) {
       }
   }
 };
+// 535. TinyURL 的加密与解密 基础映射
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+ var encode = function(longUrl) {
+  this.dataBase = new Map();
+  this.id = 0;
+  this.id++;
+  this.dataBase.set(this.id, longUrl);
+  return "http://tinyurl.com/" + this.id;
+};
+
+var decode = function(shortUrl) {
+  const p = shortUrl.lastIndexOf('/') + 1;
+  const key = parseInt(shortUrl.substring(p));
+  return this.dataBase.get(key);
+};
