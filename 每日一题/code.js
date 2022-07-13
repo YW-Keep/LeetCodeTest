259148fe998defa0ff890cea08d3efa3b3107f88
tangyuwei <tangyuwei@myhexin.com>
2022-07-13 10:18:50 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -15141,7 +15141,7 @@ MyCalendar.prototype.book = function(start, end) {
   return ans;
 };
 
-// 1252. 奇数值单元格的数目
+// 1252. 奇数值单元格的数目 奇偶判断
 /**
  * @param {number} m
  * @param {number} n
@@ -15167,4 +15167,27 @@ MyCalendar.prototype.book = function(start, end) {
       }
   }
   return oddx * (n - oddy) + (m - oddx) * oddy;
+};
+
+// 735. 行星碰撞 栈
+var asteroidCollision = function(asteroids) {
+  const stack = [];
+  for (const aster of asteroids) {
+      let alive = true;
+      while (alive && aster < 0 && stack.length > 0 && stack[stack.length - 1] > 0) {
+          alive = stack[stack.length - 1] < -aster; // aster 是否存在
+          if (stack[stack.length - 1] <= -aster) {  // 栈顶行星爆炸
+              stack.pop();
+          }
+      }
+      if (alive) {
+          stack.push(aster);
+      }
+  }
+  const size = stack.length;
+  const ans = new Array(size).fill(0);
+  for (let i = size - 1; i >= 0; i--) {
+      ans[i] = stack.pop();
+  }
+  return ans;
 };
