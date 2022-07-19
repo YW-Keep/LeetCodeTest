7d776138bfbca0d4590fd9e05f29326e24b8941a
tangyuwei <tangyuwei@myhexin.com>
2022-07-19 09:19:28 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -15275,4 +15275,25 @@ MovingAverage.prototype.next = function(val) {
   return this.sum / this.queue.length;
 };
 
+// 731. 我的日程安排表 II 直接遍历
+var MyCalendarTwo = function() {
+  this.booked = [];
+  this.overlaps = [];
+};
 
+MyCalendarTwo.prototype.book = function(start, end) {
+  for (const arr of this.overlaps) {
+      let l = arr[0], r = arr[1];
+      if (l < end && start < r) {
+          return false;
+      }
+  }
+  for (const arr of this.booked) {
+      let l = arr[0], r = arr[1];
+      if (l < end && start < r) {
+          this.overlaps.push([Math.max(l, start), Math.min(r, end)]);
+      }
+  }
+  this.booked.push([start, end]);
+  return true;
+};
