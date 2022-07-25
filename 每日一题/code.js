4bcc10e61dc471184206a258d7ab1d9d1bd7b895
tangyuwei <tangyuwei@myhexin.com>
2022-07-25 10:19:12 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -15386,3 +15386,27 @@ MyCalendarTwo.prototype.book = function(start, end) {
   }
   return res;
 };
+
+// 1184. 公交站间的距离  正反计算
+/**
+ * @param {number[]} distance
+ * @param {number} start
+ * @param {number} destination
+ * @return {number}
+ */
+ var distanceBetweenBusStops = function(distance, start, destination) {
+  if (start > destination) {
+      const temp = start;
+      start = destination;
+      destination = temp;
+  }
+  let sum1 = 0, sum2 = 0;
+  for (let i = 0; i < distance.length; i++) {
+      if (i >= start && i < destination) {
+          sum1 += distance[i];
+      } else {
+          sum2 += distance[i];
+      }
+  }
+  return Math.min(sum1, sum2);
+};
