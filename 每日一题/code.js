8d3156000511476627bc9f3490af4a6bb3105ca2
tangyuwei <tangyuwei@myhexin.com>
2022-07-28 09:49:12 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -15464,3 +15464,25 @@ MyCalendarTwo.prototype.book = function(start, end) {
   }
 
 }
+
+// 1331. 数组序号转换 排序 映射
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+ var arrayRankTransform = function(arr) {
+  const sortedArr = new Array(arr.length).fill(0);
+  sortedArr.splice(0, arr.length, ...arr);
+  sortedArr.sort((a, b) => a - b);
+  const ranks = new Map();
+  const ans = new Array(arr.length).fill(0);
+  for (const a of sortedArr) {
+      if (!ranks.has(a)) {
+          ranks.set(a, ranks.size + 1);
+      }
+  }
+  for (let i = 0; i < arr.length; i++) {
+      ans[i] = ranks.get(arr[i]);
+  }
+  return ans;
+};
