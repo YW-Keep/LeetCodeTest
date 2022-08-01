5f5acdfa6540d8130ed61f71a7ea33ad07267c87
tangyuwei <tangyuwei@myhexin.com>
2022-08-01 09:33:20 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -15537,3 +15537,17 @@ MyCalendarTwo.prototype.book = function(start, end) {
       return (v1[0] * v2[0] + v1[1] * v2[1]) === 0;
   };
 }
+
+// 1374. 生成每种字符都是奇数个的字符串 脑筋急转弯
+/**
+ * @param {number} n
+ * @return {string}
+ */
+ var generateTheString = function(n) {
+  const sb = '';
+  if (n % 2 === 1) {
+      return sb + 'a'.repeat(n);;
+  }
+  return sb + 'a'.repeat(n - 1) + 'b';
+};
+
