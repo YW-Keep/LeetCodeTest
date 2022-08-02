69dc52cd1fa1e8dd3f3b10ebf65c2344d8305899
tangyuwei <tangyuwei@myhexin.com>
2022-08-02 10:35:03 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -15551,3 +15551,52 @@ MyCalendarTwo.prototype.book = function(start, end) {
   return sb + 'a'.repeat(n - 1) + 'b';
 };
 
+// 622. 设计循环队列 基础
+/**
+ * @param {number} k
+ */
+var MyCircularQueue = function(k) {
+    this.capacity = k + 1;
+    this.elements = new Array(this.capacity).fill(0);
+    this.rear = 0;
+    this.front = 0;
+};
+
+MyCircularQueue.prototype.enQueue = function(value) {
+    if (this.isFull()) {
+        return false;
+    }
+    this.elements[this.rear] = value;
+    this.rear = (this.rear + 1) % this.capacity;
+    return true;
+};
+
+MyCircularQueue.prototype.deQueue = function() {
+    if (this.isEmpty()) {
+        return false;
+    }
+    this.front = (this.front + 1) % this.capacity;
+    return true;
+};
+
+MyCircularQueue.prototype.Front = function() {
+    if (this.isEmpty()) {
+        return -1;
+    }
+    return this.elements[this.front];
+};
+
+MyCircularQueue.prototype.Rear = function() {
+    if (this.isEmpty()) {
+        return -1;
+    }
+    return this.elements[(this.rear - 1 + this.capacity) % this.capacity];
+};
+
+MyCircularQueue.prototype.isEmpty = function() {
+    return this.rear == this.front;
+};
+
+MyCircularQueue.prototype.isFull = function() {
+    return ((this.rear + 1) % this.capacity) === this.front;
+};
