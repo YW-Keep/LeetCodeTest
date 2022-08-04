b2f2248007b8da5095733598150d30a09fe3b4e9
tangyuwei <tangyuwei@myhexin.com>
2022-08-04 09:51:50 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -15621,3 +15621,22 @@ MyCircularQueue.prototype.isFull = function() {
   return [...s].sort().join('');
 };
 
+// 1403. 非递增顺序的最小子序列 排序 贪心
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var minSubsequence = function(nums) {
+  const total = _.sum(nums);
+  nums.sort((a, b) => a - b);
+  const ans = [];
+  let curr = 0;
+  for (let i = nums.length - 1; i >= 0; --i) {
+      curr += nums[i];
+      ans.push(nums[i]);
+      if (total - curr < curr) {
+          break;
+      }
+  }
+  return ans;
+};
