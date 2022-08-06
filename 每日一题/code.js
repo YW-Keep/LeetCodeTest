157a34e6715db52beeb7acd4c8be4a0dc22ddc45
tangyuwei <tangyuwei@myhexin.com>
2022-08-06 09:44:25 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -15665,3 +15665,20 @@ MyCircularQueue.prototype.isFull = function() {
   return root;
 };
 
+// 1408. 数组中的字符串匹配  暴力拆解
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+ var stringMatching = function(words) {
+  const ret = [];
+  for (let i = 0; i < words.length; i++) {
+      for (let j = 0; j < words.length; j++) {
+          if (i !== j && words[j].search(words[i]) !== -1) {
+              ret.push(words[i]);
+              break;
+          }
+      }
+  }
+  return ret;
+};
