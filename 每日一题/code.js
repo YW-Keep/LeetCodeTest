6c18af1cc3de2ea4667cece64f175f81cae6d08b
tangyuwei <tangyuwei@myhexin.com>
2022-08-17 09:48:49 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -15835,3 +15835,37 @@ OrderedStream.prototype.insert = function(id, value) {
   }
   return res;
 };
+
+// 1302. 层数最深叶子节点的和 层序遍历
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var deepestLeavesSum = function(root) {
+  let sum = 0;
+  const queue = [];
+  queue.push(root);
+  while (queue.length) {
+      sum = 0;
+      const size = queue.length;
+      for (let i = 0; i < size; i++) {
+          const node = queue.shift();
+          sum += node.val;
+          if (node.left) {
+              queue.push(node.left);
+          }
+          if (node.right) {
+              queue.push(node.right);
+          }
+      }
+  }
+  return sum;
+};
