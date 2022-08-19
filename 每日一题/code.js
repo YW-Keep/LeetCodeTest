b496c42f6dd564b38a115952a9d3cc7cbba99970
tangyuwei <tangyuwei@myhexin.com>
2022-08-19 09:12:37 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -15869,3 +15869,22 @@ OrderedStream.prototype.insert = function(id, value) {
   }
   return sum;
 };
+
+// 1450. 在既定时间做作业的学生人数 基础题
+/**
+ * @param {number[]} startTime
+ * @param {number[]} endTime
+ * @param {number} queryTime
+ * @return {number}
+ */
+
+ var busyStudent = function(startTime, endTime, queryTime) {
+  const n = startTime.length;
+  let ans = 0;
+  for (let i = 0; i < n; i++) {
+      if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+          ans++;
+      }
+  }
+  return ans;
+};
