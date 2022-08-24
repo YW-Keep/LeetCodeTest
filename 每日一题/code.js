123ea70241ee8f04cffff200bbe36bbd3f1de163
tangyuwei <tangyuwei@myhexin.com>
2022-08-24 09:15:08 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -15931,3 +15931,30 @@ OrderedStream.prototype.insert = function(id, value) {
   dfs(res, root, 0, Math.floor((n - 1) / 2), height);
   return res;
 };
+
+// 1460. 通过翻转子数组使两个数组相等 map映射
+/**
+ * @param {number[]} target
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+
+ var canBeEqual = function(target, arr) {
+  const counts1 = new Map();
+  const counts2 = new Map();
+  for (const num of target) {
+      counts1.set(num, (counts1.get(num) || 0) + 1);
+  }
+  for (const num of arr) {
+      counts2.set(num, (counts2.get(num) || 0) + 1);   
+  }
+  if (counts1.size !== counts2.size) {
+      return false;
+  }
+  for (const [key, value] of counts1.entries()) {
+      if (!counts2.has(key) || counts2.get(key) !== value) {
+          return false;
+      }
+  }
+  return true;
+};
