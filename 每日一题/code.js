13b7b78f77d7d9a50c072cbdaf0c11748611ea61
tangyuwei <tangyuwei@myhexin.com>
2022-08-29 09:02:55 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -16000,3 +16000,13 @@ OrderedStream.prototype.insert = function(id, value) {
       return low;
   }
 }
+
+// 1470. 重新排列数组 基础题
+var shuffle = function(nums, n) {
+  const ans = new Array(2 * n).fill(0);
+  for (let i = 0; i < n; i++) {
+      ans[2 * i] = nums[i];
+      ans[2 * i + 1] = nums[i + n];
+  }
+  return ans;
+};
