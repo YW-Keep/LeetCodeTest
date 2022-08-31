a4ab23d45c2ddbfa90c4005384838942c6feccf3
tangyuwei <tangyuwei@myhexin.com>
2022-08-31 09:47:36 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -16044,3 +16044,22 @@ var shuffle = function(nums, n) {
   parent.right = new TreeNode(val);
   return root;
 };
+
+// 946. 验证栈序列 模拟
+/**
+ * @param {number[]} pushed
+ * @param {number[]} popped
+ * @return {boolean}
+ */
+ var validateStackSequences = function(pushed, popped) {
+  const stack = [];
+  const n = pushed.length;
+  for (let i = 0, j = 0; i < n; i++) {
+      stack.push(pushed[i]);
+      while (stack.length && stack[stack.length - 1] == popped[j]) {
+          stack.pop();
+          j++;
+      }
+  }
+  return stack.length === 0;
+};
