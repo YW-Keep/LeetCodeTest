42755ce40654c63769b8a6963da34eafd4828301
tangyuwei <tangyuwei@myhexin.com>
2022-09-01 09:53:03 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -16063,3 +16063,23 @@ var shuffle = function(nums, n) {
   }
   return stack.length === 0;
 };
+
+// 1475. 商品折扣后的最终价格 单调栈
+/**
+ * @param {number[]} prices
+ * @return {number[]}
+ */
+ var finalPrices = function(prices) {
+  const n = prices.length;
+  const ans = new Array(n).fill(0);
+  const stack = [];
+  for (let i = n - 1; i >= 0; i--) {
+      while (stack.length && stack[stack.length - 1] > prices[i]) {
+          stack.pop();
+      }
+      ans[i] = stack.length === 0 ? prices[i] : prices[i] - stack[stack.length - 1];
+      stack.push(prices[i]);
+  }
+  return ans;
+};  
+
