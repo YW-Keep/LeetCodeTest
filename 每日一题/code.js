574b4927873a551c58ed62f2bed02ffbeb54d1a9
tangyuwei <tangyuwei@myhexin.com>
2022-09-06 10:08:47 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -16164,3 +16164,28 @@ var shuffle = function(nums, n) {
   dfs(root);
   return [...repeat];
 };
+
+// 828. 统计子串中的唯一字符  分别计算每个字符的贡献
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var uniqueLetterString = function(s) {
+  const index = new Map();
+      for (let i = 0; i < s.length; i++) {
+          const c = s[i];
+          if (!index.has(c)) {
+              index.set(c, []);
+              index.get(c).push(-1);
+          }
+          index.get(c).push(i);
+      }
+      let res = 0;
+      for (const [_, arr] of index.entries()) {
+          arr.push(s.length);
+          for (let i = 1; i < arr.length - 1; i++) {
+              res += (arr[i] - arr[i - 1]) * (arr[i + 1] - arr[i]);
+          }
+      }
+      return res;
+};
