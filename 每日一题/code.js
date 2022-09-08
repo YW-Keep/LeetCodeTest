42f11b0dd7b8bb3d85d582fabfad42225d95957c
tangyuwei <tangyuwei@myhexin.com>
2022-09-08 09:48:52 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -16232,3 +16232,28 @@ var shuffle = function(nums, n) {
   }
   return sb;
 };
+
+// 667. 优美的排列 II 基础逻辑
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[]}
+ */
+
+ var constructArray = function(n, k) {
+  const answer = new Array(n).fill(0);
+  let idx = 0;
+  for (let i = 1; i < n - k; ++i) {
+      answer[idx] = i;
+      ++idx;
+  }
+  for (let i = n - k, j = n; i <= j; ++i, --j) {
+      answer[idx] = i;
+      ++idx;
+      if (i !== j) {
+          answer[idx] = j;
+          ++idx;
+      }
+  }
+  return answer;
+};
