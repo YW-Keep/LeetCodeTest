71ee4376d1b22ac29e6bf5dfdae86cf2b18478b8
tangyuwei <tangyuwei@myhexin.com>
2022-09-13 10:00:40 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -16278,3 +16278,36 @@ var shuffle = function(nums, n) {
   }
   return depth;
 };
+
+// 670. 最大交换  贪心算法
+/**
+ * @param {number} num
+ * @return {number}
+ */
+ var maximumSwap = function(num) {
+  const charArray = [...'' + num];
+  const n = charArray.length;
+  let maxIdx = n - 1;
+  let idx1 = -1, idx2 = -1;
+  for (let i = n - 1; i >= 0; i--) {
+      if (charArray[i] > charArray[maxIdx]) {
+          maxIdx = i;
+      } else if (charArray[i] < charArray[maxIdx]) {
+          idx1 = i;
+          idx2 = maxIdx;
+      }
+  }
+  if (idx1 >= 0) {
+      swap(charArray, idx1, idx2);
+      return parseInt(charArray.join(''));
+  } else {
+      return num;
+  }
+  function swap(charArray, i, j) {
+      const temp = charArray[i];
+      charArray[i] = charArray[j];
+      charArray[j] = temp;
+  };
+}
+
+
