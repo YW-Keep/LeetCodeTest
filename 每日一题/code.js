f06c13edc8a482dec93483c10de6865129f8ea79
tangyuwei <tangyuwei@myhexin.com>
2022-09-22 13:47:48 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -16382,3 +16382,30 @@ var shuffle = function(nums, n) {
 
   return list;
 };
+
+// 1640. 能否连接形成数组  哈希表
+/**
+ * @param {number[]} arr
+ * @param {number[][]} pieces
+ * @return {boolean}
+ */
+ var canFormArray = function(arr, pieces) {
+  const n = arr.length, m = pieces.length;
+  const index = new Map();
+  for (let i = 0; i < m; i++) {
+      index.set(pieces[i][0], i);
+  }
+  for (let i = 0; i < n;) {
+      if (!index.has(arr[i])) {
+          return false;
+      }
+      const j = index.get(arr[i]), len = pieces[j].length;
+      for (let k = 0; k < len; k++) {
+          if (arr[i + k] != pieces[j][k]) {
+              return false;
+          }
+      }
+      i = i + len;
+  }
+  return true;
+};
