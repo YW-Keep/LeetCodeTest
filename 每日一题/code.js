101d043ae578d2da2f54fb970e87ed29ff50df86
tangyuwei <tangyuwei@myhexin.com>
2022-10-11 09:44:30 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -16494,3 +16494,29 @@ var shuffle = function(nums, n) {
   }
   return ans;
 };
+
+// 1790. 仅执行一次字符串交换能否使两个字符串相等 逻辑判断
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+ var areAlmostEqual = function(s1, s2) {
+  const n = s1.length;
+  const diff = [];
+  for (let i = 0; i < n; ++i) {
+      if (s1[i] !== s2[i]) {
+          if (diff.length >= 2) {
+              return false;
+          }
+          diff.push(i);
+      }
+  }
+  if (diff.length === 0) {
+      return true;
+  }
+  if (diff.length !== 2) {
+      return false;
+  }
+  return s1[diff[0]] === s2[diff[1]] && s1[diff[1]] === s2[diff[0]];
+};
