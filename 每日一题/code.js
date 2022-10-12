c53eed1f2b4d9dd105c601b61de9f1aeb46b6f00
tangyuwei <tangyuwei@myhexin.com>
2022-10-12 09:49:54 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -16520,3 +16520,38 @@ var shuffle = function(nums, n) {
   }
   return s1[diff[0]] === s2[diff[1]] && s1[diff[1]] === s2[diff[0]];
 };
+
+
+// 817. 链表组件 map 遍历
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var numComponents = function(head, nums) {
+  const numsSet = new Set();
+  for (const num of nums) {
+      numsSet.add(num);
+  }
+  let inSet = false;
+  let res = 0;
+  while (head) {
+      if (numsSet.has(head.val)) {
+          if (!inSet) {
+              inSet = true;
+              res++;
+          }
+      } else {
+          inSet = false;
+      }
+      head = head.next;
+  }
+  return res;
+};
