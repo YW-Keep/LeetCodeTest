6838422abefdc9753e2fd3022da59e72ce88c664
tangyuwei <tangyuwei@myhexin.com>
2022-10-17 09:47:11 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -16590,3 +16590,23 @@ var shuffle = function(nums, n) {
   }
   return total;
 };
+
+// 904. 水果成篮 滑动窗口
+var totalFruit = function(fruits) {
+  const n = fruits.length;
+  const cnt = new Map();
+
+  let left = 0, ans = 0;
+  for (let right = 0; right < n; ++right) {
+      cnt.set(fruits[right], (cnt.get(fruits[right]) || 0) + 1);
+      while (cnt.size > 2) {
+          cnt.set(fruits[left], cnt.get(fruits[left]) - 1);
+          if (cnt.get(fruits[left]) == 0) {
+              cnt.delete(fruits[left]);
+          }
+          ++left;
+      }
+      ans = Math.max(ans, right - left + 1);
+  }
+  return ans;
+};
