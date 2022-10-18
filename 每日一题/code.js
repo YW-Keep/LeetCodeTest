409fe4aa1a01a4a97b0c094da99c606e41913655
tangyuwei <tangyuwei@myhexin.com>
2022-10-18 10:18:38 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -16610,3 +16610,32 @@ var totalFruit = function(fruits) {
   }
   return ans;
 };
+
+// // 902. 最大为 N 的数字组合  动态规划
+/**
+ * @param {string[]} digits
+ * @param {number} n
+ * @return {number}
+ */
+ var atMostNGivenDigitSet = function(digits, n) {
+  const s = '' + n;
+  const m = digits.length, k = s.length;
+  const dp = new Array(k + 1).fill(0).map(() => new Array(2).fill(0));
+  dp[0][1] = 1;
+  for (let i = 1; i <= k; i++) {
+      for (let j = 0; j < m; j++) {
+          if (digits[j][0] === s[i - 1]) {
+              dp[i][1] = dp[i - 1][1];
+          } else if (digits[j][0] < s[i - 1]) {
+              dp[i][0] += dp[i - 1][1];
+          } else {
+              break;
+          }
+      }
+      if (i > 1) {
+          dp[i][0] += m + dp[i - 1][0] * m;
+      }
+  }
+  return dp[k][0] + dp[k][1];
+};
+
