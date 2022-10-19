7a924deb3ff7fd5b65cce87595a7131f1d123c01
tangyuwei <tangyuwei@myhexin.com>
2022-10-19 09:44:22 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -16639,3 +16639,23 @@ var totalFruit = function(fruits) {
   return dp[k][0] + dp[k][1];
 };
 
+// 1700. 无法吃午餐的学生数量 基础题
+/**
+ * @param {number[]} students
+ * @param {number[]} sandwiches
+ * @return {number}
+ */
+ var countStudents = function(students, sandwiches) {
+  let s1 = _.sum(students);
+  let s0 = students.length - s1;
+  for (let i = 0; i < sandwiches.length; i++) {
+      if (sandwiches[i] === 0 && s0 > 0) {
+          s0--;
+      } else if (sandwiches[i] === 1 && s1 > 0) {
+          s1--;
+      } else {
+          break;
+      }
+  }
+  return s0 + s1;
+};
