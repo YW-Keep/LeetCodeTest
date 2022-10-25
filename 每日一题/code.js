0844ddca7c3784630effb5113cb971ee9324d653
tangyuwei <tangyuwei@myhexin.com>
2022-10-25 14:51:00 +0800

    更js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -16659,3 +16659,28 @@ var totalFruit = function(fruits) {
   }
   return s0 + s1;
 };
+
+// 1768. 交替合并字符串 双指针
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+
+ var mergeAlternately = function(word1, word2) {
+  const m = word1.length, n = word2.length;
+  let i = 0, j = 0;
+
+  const ans = [];
+  while (i < m || j < n) {
+      if (i < m) {
+          ans.push(word1[i]);
+          ++i;
+      }
+      if (j < n) {
+          ans.push(word2[j]);
+          ++j;
+      }
+  }
+  return ans.join('');
+};
