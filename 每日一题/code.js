0a25c00cae093efdd26ad58ec7ecf4f3fcd06340
tangyuwei <tangyuwei@myhexin.com>
2022-10-26 16:27:39 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -16684,3 +16684,30 @@ var totalFruit = function(fruits) {
   }
   return ans.join('');
 };
+
+// 862. 和至少为 K 的最短子数组 队列
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+ var shortestSubarray = function(nums, k) {
+  const n = nums.length;
+  const preSumArr = new Array(n + 1).fill(0);
+      for (let i = 0; i < n; i++) {
+      preSumArr[i + 1] = preSumArr[i] + nums[i];
+  }
+  let res = n + 1;
+  const queue = [];
+  for (let i = 0; i <= n; i++) {
+      const curSum = preSumArr[i];
+      while (queue.length != 0 && curSum - preSumArr[queue[0]] >= k) {
+          res = Math.min(res, i - queue.shift());
+      }
+      while (queue.length != 0 && preSumArr[queue[queue.length - 1]] >= curSum) {
+          queue.pop();
+      }
+      queue.push(i);
+  }
+  return res < n + 1 ? res : -1;
+};
