e2ba751e225e0825187abd62bbaed4d79fc32d6a
tangyuwei <tangyuwei@myhexin.com>
2022-10-28 09:41:04 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -16728,3 +16728,27 @@ var totalFruit = function(fruits) {
   }
   return count%2 == 1 ? -1 : 1
 };
+
+// 907. 子数组的最小值之和 动态规划
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+
+ var sumSubarrayMins = function(arr) {
+  const n = arr.length;
+  let ans = 0;
+  const MOD = 1000000007;
+  const monoStack = [];
+  const dp = new Array(n).fill(0);
+  for (let i = 0; i < n; i++) {
+      while (monoStack.length !== 0 && arr[monoStack[monoStack.length - 1]] > arr[i]) {
+          monoStack.pop();
+      }
+      const k = monoStack.length === 0 ? (i + 1) : (i - monoStack[monoStack.length - 1]);
+      dp[i] = k * arr[i] + (monoStack.length === 0 ? 0 : dp[i - k]);
+      ans = (ans + dp[i]) % MOD;
+      monoStack.push(i);
+  }
+  return ans;
+};
