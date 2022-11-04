b89f328d22a6099d212146bae6e2c28283e1492e
tangyuwei <tangyuwei@myhexin.com>
2022-11-04 10:32:02 +0800

    更js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -16836,3 +16836,19 @@ var totalFruit = function(fruits) {
 
   return k
 };
+
+// 754. 到达终点数字 数学题
+/**
+ * @param {number} target
+ * @return {number}
+ */
+ var reachNumber = function(target) {
+  target = Math.abs(target);
+  let k = 0;
+  while (target > 0) {
+      k++;
+      target -= k;
+  }
+  return target % 2 === 0 ? k : k + 1 + k % 2;
+};
+
