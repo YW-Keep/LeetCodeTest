8a589ef44398749bd629ec6835d1cd9706d9fb12
tangyuwei <tangyuwei@myhexin.com>
2022-11-07 09:54:27 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -16894,3 +16894,24 @@ var totalFruit = function(fruits) {
   }
   return stack.pop() === 't';
 };
+
+// 1678. 设计 Goal 解析器 基础遍历
+/**
+ * @param {string} command
+ * @return {string}
+ */
+ var interpret = function(command) {
+  let res = '';
+  for (let i = 0; i < command.length; i++) {
+      if (command[i] === 'G') {
+          res += 'G';
+      } else if (command[i] === '(') {
+          if (command[i + 1] === ')') {
+              res += 'o';
+          } else {
+              res += 'al';
+          }
+      }
+  }
+  return res;
+};
