178771405819132a8d1c45cbbffda1cc95db55f6
tangyuwei <tangyuwei@myhexin.com>
2022-11-08 10:26:40 +0800

    更新js

--- 每日一题/code.js
+++ 每日一题/code.js
@@ -16915,3 +16915,30 @@ var totalFruit = function(fruits) {
   }
   return res;
 };
+
+// 1684. 统计一致字符串的数目  基础遍历位运算
+/**
+ * @param {string} allowed
+ * @param {string[]} words
+ * @return {number}
+ */
+ var countConsistentStrings = function(allowed, words) {
+  let mask = 0;
+  for (let i = 0; i < allowed.length; i++) {
+      const c = allowed[i];
+      mask |= 1 << (c.charCodeAt() - 'a'.charCodeAt());
+  }
+  let res = 0;
+  for (const word of words) {
+      let mask1 = 0;
+      for (let i = 0; i < word.length; i++) {
+          const c = word[i];
+          mask1 |= 1 << (c.charCodeAt() - 'a'.charCodeAt());
+      }
+      if ((mask1 | mask) === mask) {
+          res++;
+      }
+  }
+  return res;
+};
+
