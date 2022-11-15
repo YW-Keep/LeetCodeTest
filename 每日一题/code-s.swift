8207670b94317d85459caa2006edaadbb2dcac8a
tangyuwei <tangyuwei@myhexin.com>
2022-11-15 09:58:45 +0800

    更新js

--- 每日一题/code-s.swift
+++ 每日一题/code-s.swift
@@ -22,7 +22,6 @@ class Solution {
 }
 
 
-
 // 791. 自定义字符串排序 字典
 class Solution791 {
     func customSortString(_ order: String, _ s: String) -> String {
@@ -48,5 +47,28 @@ class Solution791 {
 
         return res 
 
+    }
+}
+
+// 1710. 卡车上的最大单元数 排序贪心
+class Solution {
+    func maximumUnits(_ boxTypes: [[Int]], _ truckSize: Int) -> Int {
+        let nBox = boxTypes.sorted { (list1, list2) -> Bool in
+            return list1[1] > list2[1]
+        }
+        
+        var result = 0
+        var n = 0
+        for list in nBox {
+            if n + list[0] <= truckSize {
+                n += list[0]
+                result += list[0] * list[1]
+            } else {
+                result += (truckSize - n) * list[1]
+                n = truckSize
+                break;
+            }
+        }
+        return result
     }
 }
