6692239649d772f46615c906c813021d28ea7ae8
tangyuwei <tangyuwei@myhexin.com>
2023-02-06 10:14:28 +0800

    更新js

--- 每日一题/code-s.swift
+++ 每日一题/code-s.swift
@@ -988,3 +988,23 @@ class Solution {
         return  String(message.map {map[$0] ?? " "})
     }
 }
+
+// 2331. 计算布尔二叉树的值 递归
+class Solution {
+    func evaluateTree(_ root: TreeNode?) -> Bool {
+        guard let root = root else {
+            return false
+        }
+        
+        if root.left == nil,
+           root.right == nil {
+            return root.val == 1
+        }
+        
+        if root.val == 2 {
+            return evaluateTree(root.left) || evaluateTree(root.right)
+        }
+        
+        return evaluateTree(root.left) && evaluateTree(root.right)
+    }
+}
