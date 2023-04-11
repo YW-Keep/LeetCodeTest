00bdfae0c36dc27f32b995aae4132e1ba873d9f7
tangyuwei <tangyuwei@myhexin.com>
2023-04-11 10:00:58 +0800

    更新代码

--- 每日一题/code-s.swift
+++ 每日一题/code-s.swift
@@ -1506,3 +1506,25 @@ class Solution {
         return result
     }
 }
+
+// 1041. 困于环中的机器人 基础逻辑题
+class Solution {
+    func isRobotBounded(_ instructions: String) -> Bool {
+        let direc = [[0,1],[1,0],[0,-1],[-1,0]]
+        var x = 0,y = 0,direcIndex = 0
+        for instruction in instructions {
+            let str = String(instruction)
+            if str == "G" {
+                x += direc[direcIndex][0]
+                y += direc[direcIndex][1]
+            } else if str  ==  "L" {
+                direcIndex += 3
+                direcIndex %= 4
+            } else {
+                direcIndex += 1
+                direcIndex %= 4
+            }
+        }
+        return direcIndex != 0 || (x == 0 && y == 0)
+    }
+}
