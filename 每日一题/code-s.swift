822e513098a169e2565e7e9c047f7ea36b178ed3
tangyuwei <tangyuwei@myhexin.com>
2023-10-23 11:09:23 +0800

    更新代码

--- 每日一题/code-s.swift
+++ 每日一题/code-s.swift
@@ -3416,3 +3416,18 @@ class Solution {
           }
     }
 }
+
+// 2678. 老人的数目 基础逻辑
+class Solution {
+    func countSeniors(_ details: [String]) -> Int {
+        var count = 0
+        for info in details {
+            let index1 = info.index(info.startIndex, offsetBy: 11) 
+            let index2 = info.index(info.startIndex, offsetBy: 12) 
+            if let age = Int(info[index1...index2]), age > 60 {
+                count += 1
+            }
+        }
+        return count
+    }
+}
