06f4783eed88a55736d4adf5e9cf6dc36f7160fd
tangyuwei <tangyuwei@myhexin.com>
2024-01-03 10:39:10 +0800

    更新代码

--- 每日一题/code-s.swift
+++ 每日一题/code-s.swift
@@ -3922,3 +3922,28 @@ class Solution {
     }
 
 }
+
+// 2487. 从链表中移除节点 递归
+public class ListNode {
+  public var val: Int
+  public var next: ListNode?
+  public init() { self.val = 0; self.next = nil; }
+  public init(_ val: Int) { self.val = val; self.next = nil; }
+  public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+class Solution {
+    func removeNodes(_ head: ListNode?) -> ListNode? {
+        guard let head = head else {
+            return head
+        }
+        head.next =  removeNodes(head.next)
+        
+        if let next = head.next, head.val < next.val {
+            return next
+        } else {
+            return head
+        }
+        
+    }
+}
