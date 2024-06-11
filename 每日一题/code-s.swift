e4bfb039d0bf51aea97d04f73d95a10607128a7b
tangyuwei <tangyuwei@myhexin.com>
2024-06-11 13:35:42 +0800

    更新代码

--- 每日一题/code-s.swift
+++ 每日一题/code-s.swift
@@ -4762,3 +4762,17 @@ class Solution {
     }
 }
 
+// 419. 甲板上的战舰 基础逻辑
+class Solution {
+    func countBattleships(_ board: [[Character]]) -> Int {
+        var ans = 0
+        for i in 0 ..< board.count {
+            for j in 0 ..< board[0].count {
+                if board[i][j] == "X", i <= 0 || board[i - 1][j] == ".", j <= 0 || board[i][j - 1] == "." {
+                    ans += 1
+                }
+            }
+        }
+        return ans
+    }
+}
